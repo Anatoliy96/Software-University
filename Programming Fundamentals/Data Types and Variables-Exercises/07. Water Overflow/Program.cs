a110847e91e7639f18241e25d5137f067627de3e
Anatoliy <leaved@abv.bg>
2022-11-15 18:13:51 +0200

    Data Types and Variables-Exercises

---
+++ Programming Fundamentals/Data Types and Variables-Exercises/07. Water Overflow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int totalLiters = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int liters = int.Parse(Console.ReadLine());
+
+                totalLiters += liters;
+
+                if (totalLiters > 255)
+                {
+                    totalLiters -= liters;
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+
+            }
+
+            Console.WriteLine($"{totalLiters}");
+        }
+    }
+}
