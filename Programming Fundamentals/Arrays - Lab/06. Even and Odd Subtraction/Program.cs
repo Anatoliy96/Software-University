62045b9a14dc44416c88f0b5e02194768bcd738d
Anatoliy <leaved@abv.bg>
2022-11-23 14:59:05 +0200

    Arrays-Lecture

---
+++ Programming Fundamentals/Arrays - Lab/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr =
+                Console.ReadLine().Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int evenSum = 0;
+            int oddSum = 0;
+            int diffrence = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    evenSum += arr[i];
+                }
+                else
+                {
+                    oddSum += arr[i];
+                }
+            }
+
+            diffrence = evenSum - oddSum;
+
+            Console.WriteLine(diffrence);
+        }
+    }
+}
