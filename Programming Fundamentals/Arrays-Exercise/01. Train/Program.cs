abeb732551bae1192473564366fb061772276429
Anatoliy <leaved@abv.bg>
2022-11-24 18:11:13 +0200

    first five assignments from array exercises.

---
+++ Programming Fundamentals/Arrays-Exercise/01. Train/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            int totalPassengers = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}" + " ");
+
+                totalPassengers += arr[i];
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(totalPassengers);
+        }
+    }
+}
