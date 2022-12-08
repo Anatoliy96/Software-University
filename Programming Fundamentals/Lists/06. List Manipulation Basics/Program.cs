6376abec5366eeeb299bc75e211e52fd6d31dca6
Anatoliy <leaved@abv.bg>
2022-12-08 17:26:47 +0200

    List-Lab

---
+++ Programming Fundamentals/Lists/06. List Manipulation Basics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._List_Manipulation_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
