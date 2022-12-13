6e125979287a2169864ebe2c2be9c3990a353965
Anatoliy <leaved@abv.bg>
2022-12-13 16:42:19 +0200

    List-exercises

---
+++ Programming Fundamentals/Lists - Exercise/08. Anonymous Threat/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Anonymous_Threat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "3:1")
+            {
+                string[] tokens = command.Split(" ");
+
+                string action = tokens[0];
+                int startIndex = int.Parse(tokens[1]);
+                int endIndex = int.Parse(tokens[2]);
+
+                if (endIndex > list.Count - 1 || endIndex < 0)
+                {
+                    endIndex = list.Count - 1;
+                }
+
+                if (startIndex > list.Count - 1 || startIndex < 0)
+                {
+                    startIndex = 0;
+                }
+
+                if (action == "merge")
+                {
+                    string concat = string.Empty;
+
+                    for (int i = startIndex; i <= endIndex; i++)
+                    {
+                        concat += list[i];
+                    }
+                    list.RemoveRange(startIndex, endIndex - startIndex + 1);
+                    list.Insert(startIndex, concat);
+                }
+                else if (action == "divide")
+                {
+                    List<string> divided = new List<string>();
+
+                    int partiosions = int.Parse(tokens[2]);
+                    string word = list[startIndex];
+                    list.RemoveAt(startIndex);
+                    int parts = word.Length / partiosions;
+
+                    for (int i = 0; i < partiosions; i++)
+                    {
+                        if (i == partiosions - 1)
+                        {
+                            divided.Add(word.Substring(i * parts));
+                        }
+                        else
+                        {
+                            divided.Add(word.Substring(i * parts, parts));
+                        }
+                    }
+
+                    list.InsertRange(startIndex, divided);
+                }
+            }
+            Console.WriteLine(string.Join(" ", list));
+            
+        }
+    }
+}
---
+++ Programming Fundamentals/Lists - Exercise/09. Pokemon Don't Go/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Pokemon_Don_t_Go
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> removedNumbers = new List<int>();
+            int removedNumber = 0;
+
+            while (numbers.Count != 0)
+            {
+                int index = int.Parse(Console.ReadLine());
+
+                if (index < 0)
+                {
+                    removedNumber = numbers[0];
+                    removedNumbers.Add(removedNumber);
+                    numbers.RemoveAt(0);
+                    numbers.Insert(0, numbers[numbers.Count - 1]);
+                }
+                else if (index > numbers.Count - 1)
+                {
+                    removedNumber = numbers[numbers.Count - 1];
+                    removedNumbers.Add(removedNumber);
+                    numbers.RemoveAt(numbers.Count - 1);
+                    numbers.Add(numbers[0]);
+                }
+                else
+                {
+                    removedNumber = numbers[index];
+                    removedNumbers.Add(removedNumber);
+                    numbers.RemoveAt(index);
+                }
+
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    if (numbers[i] <= removedNumber)
+                    {
+                        numbers[i] += removedNumber;
+                    }
+                    else if (numbers[i] > removedNumber)
+                    {
+                        numbers[i] -= removedNumber;
+                    }
+                }
+
+            }
+
+            Console.WriteLine(removedNumbers.Sum());
+        }
+    }
+}
