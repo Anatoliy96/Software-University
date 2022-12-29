a1dfbd6d2fe7af4207fa1d48181c0ac50dcd9b1c
Anatoliy <leaved@abv.bg>
2022-12-29 14:54:30 +0200

    Associative Arrays - lab

---
+++ Programming Fundamentals/Associative Arrays/01. Count Real Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<int, int> counts = new SortedDictionary<int, int>();
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            foreach (var number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts.Add(number, 1);
+                }
+            }
+
+            foreach (var count in counts)
+            {
+                Console.WriteLine($"{count.Key} -> {count.Value}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Associative Arrays/03. Word Synonyms/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!words.ContainsKey(word))
+                {
+                    words.Add(word, new List<string>());
+                }
+
+                words[word].Add(synonym);
+            }
+
+            foreach (var synonym in words)
+            {
+                Console.WriteLine($"{synonym.Key} - {string.Join(", ", synonym.Value)}");
+            }
+        }
+    }
+}
