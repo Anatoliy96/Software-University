fa17698c5c6e4d22574370baeadaf1baf5f522e4
Anatoliy <leaved@abv.bg>
2023-01-07 10:45:05 +0200

    Text Processing - Lab

---
+++ Programming Fundamentals/Text Processing/01. Reverse Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+
+            while ((word = Console.ReadLine()) != "end")
+            {
+                string result = "";
+
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    result += word[i];
+                }
+
+                Console.WriteLine($"{word} = {result}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Text Processing/04. Text Filter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordsToBan = Console.ReadLine().Split(", ");
+
+            string text = Console.ReadLine();
+
+            foreach (var wordToBan in wordsToBan)
+            {
+                if (text.Contains(wordToBan))
+                {
+                    text = text.Replace(wordToBan, new string('*', wordToBan.Length));
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
