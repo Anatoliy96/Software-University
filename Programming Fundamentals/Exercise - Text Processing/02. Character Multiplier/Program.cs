6b3d5b3134a79f516c61c22a4b81427aff41fbd4
Anatoliy <leaved@abv.bg>
2023-01-09 14:53:28 +0200

    Text Processing - Exercises

---
+++ Programming Fundamentals/Exercise - Text Processing/02. Character Multiplier/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _02._Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            string text1 = input[0];
+            string text2 = input[1];
+
+            int result = SumOfCharacters(text1, text2);
+            Console.WriteLine(result);
+        }
+
+        public static int SumOfCharacters(string text1, string text2)
+        {
+            int totalSum = 0;
+            string longest = "";
+            string shortest = "";
+
+            if (text1.Length > text2.Length)
+            {
+                longest = text1;
+                shortest = text2;
+            }
+            else
+            {
+                longest = text2;
+                shortest = text1;
+            }
+
+            for (int i = 0; i < shortest.Length; i++)
+            {
+                totalSum += text1[i] * text2[i];
+                
+            }
+            for (int i = shortest.Length; i < longest.Length; i++)
+            {
+                totalSum += longest[i];
+            }
+            
+
+            return totalSum;
+        }
+    }
+}
---
+++ Programming Fundamentals/Exercise - Text Processing/05. Multiply Big Number/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Multiply_Big_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string reallyBigInt = Console.ReadLine();
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            int remainder = 0;
+
+            for (int i = reallyBigInt.Length - 1; i >= 0; i--)
+            {
+                char lastChar = reallyBigInt[i];
+                int lastNum = int.Parse(lastChar.ToString());
+
+                int result = lastNum * number + remainder;
+
+                sb.Append(lastChar % 10);
+
+                remainder = result / 10;
+            }
+
+            if (remainder != 0)
+            {
+                sb.Append(remainder);
+            }
+
+            StringBuilder reversedSb = new StringBuilder();
+
+            for (int i = sb.Length - 1; i >= 0; i--)
+            {
+                reversedSb.Append(sb[i]);
+            }
+
+            Console.WriteLine(reversedSb);
+        }
+    }
+}
