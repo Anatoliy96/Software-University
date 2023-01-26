4ee753e11556dd551911e6f227c946660d5aa726
Anatoliy <leaved@abv.bg>
2023-01-26 13:00:15 +0200

    Arrays - Exercise

---
+++ Programming Fundamentals pt2/Arrays - Exercise/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Zig_Zag_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            int[] arr1 = new int[rows];
+            int[] arr2 = new int[rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                if (i % 2 == 0)
+                {
+                    arr1[i] = numbers[0];
+                    arr2[i] = numbers[1];
+                }
+                else
+                {
+                    arr1[i] = numbers[1];
+                    arr2[i] = numbers[0];
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", arr1));
+            Console.WriteLine(String.Join(" ", arr2));
+        }
+    }
+}
---
+++ Programming Fundamentals pt2/Arrays - Exercise/04. Array Rotation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int rotations = int.Parse(Console.ReadLine());
+
+            int firstNumber = 0;
+
+            for (int i = 0; i < rotations; i++)
+            {
+                firstNumber = numbers[0];
+
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    numbers[j] = numbers[j + 1];
+                }
+
+                numbers[numbers.Length - 1] = firstNumber;
+            }
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
