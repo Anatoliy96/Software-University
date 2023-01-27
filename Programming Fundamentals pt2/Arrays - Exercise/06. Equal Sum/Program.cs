6f737493c8137ccfd1bacf77044724ddbc3419f2
Anatoliy <leaved@abv.bg>
2023-01-27 21:45:19 +0200

    Arrays - Exercise

---
+++ Programming Fundamentals pt2/Arrays - Exercise/06. Equal Sum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr =
+                Console.ReadLine().Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += arr[j];
+                }
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    rightSum += arr[j];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+                
+            }
+
+            Console.WriteLine("no");
+        }
+    }
+}
---
+++ Programming Fundamentals pt2/Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers =
+                Console.ReadLine().Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int longestSequenceStartIndex = 0;
+            int longestSequenceLenght = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int currentSequenceStartIndex = i - 1;
+                int currentSequenceLenght = 1;
+
+                while (i < numbers.Length && numbers[i] == numbers[i - 1])
+                {
+                    currentSequenceLenght++;
+                    i++;
+                }
+
+                if (currentSequenceLenght > longestSequenceLenght)
+                {
+                    longestSequenceLenght = currentSequenceLenght;
+                    longestSequenceStartIndex = currentSequenceStartIndex;
+                }
+            }
+
+            for (int i = longestSequenceStartIndex; i < longestSequenceStartIndex + longestSequenceLenght; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+        }
+    }
+}
