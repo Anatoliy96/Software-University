b67822c7925817d665e78059488df0e4cd16c31e
Anatoliy <leaved@abv.bg>
2023-01-30 15:19:17 +0200

    Arrays - More Exercise

---
+++ Programming Fundamentals pt2/Arrays - More Exercise/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] words = new int[n];
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if (word[j] == 'a' || word[j] == 'e' || word[j] == 'i' || word[j] == 'o' || word[j] == 'u' ||
+                        word[j] == 'A' || word[j] == 'E' || word[j] == 'I' || word[j] == 'O' || word[j] == 'U')
+                    {
+                        sum += word[j] * word.Length;
+                    }
+                    else
+                    {
+                        sum += word[j] / word.Length;
+                    }
+                }
+
+                words[i] = sum;
+                sum = 0;
+            }
+            Array.Sort(words);
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
