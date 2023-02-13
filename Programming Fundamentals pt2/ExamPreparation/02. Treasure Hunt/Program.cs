6ec99bd253beb39c86cdb6631155d3d9a82ca9d9
Anatoliy <leaved@abv.bg>
2023-02-13 21:04:16 +0200

    Exam preparation

---
+++ Programming Fundamentals pt2/ExamPreparation/02. Treasure Hunt/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Treasure_Hunt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> treasure = Console.ReadLine().Split("|").ToList();
+            List<string> removedTreasure = new List<string>();
+
+            string commands = Console.ReadLine();
+
+            while (commands != "Yohoho!")
+            {
+                string[] command = commands.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "Loot")
+                {
+                    for (int i = 1; i < command.Length; i++)
+                    {
+                        if (!treasure.Contains(command[i]))
+                        {
+                            treasure.Insert(0, command[i]);
+                        }
+                    }
+                }
+                else if (command[0] == "Drop")
+                {
+                    int index = int.Parse(command[1]);
+
+                    if (index >= 0 && index < treasure.Count)
+                    {
+                        string item = treasure[index];
+                        treasure.RemoveAt(index);
+                        treasure.Add(item);
+                    }
+                }
+                else if (command[0] == "Steal")
+                {
+                    int count = int.Parse(command[1]);
+
+                    if (treasure.Count < count)
+                    {
+                        int itemsToRemove = Math.Min(count, treasure.Count);
+                        string[] items = treasure.GetRange(treasure.Count - itemsToRemove, itemsToRemove).ToArray();
+                        treasure.RemoveRange(treasure.Count - itemsToRemove, itemsToRemove);
+
+                        Console.WriteLine(String.Join(", ", items));
+                    }
+                    else
+                    {
+                        int itemsToRemove = Math.Min(count, treasure.Count);
+                        string[] items = treasure.GetRange(treasure.Count - itemsToRemove, itemsToRemove).ToArray();
+                        treasure.RemoveRange(treasure.Count - itemsToRemove, itemsToRemove);
+
+                        Console.WriteLine(String.Join(", ", items));
+                    }
+                }
+
+                commands = Console.ReadLine();
+            }
+
+            if (treasure.Count == 0)
+            {
+                Console.WriteLine("Failed treasure hunt.");
+            }
+            else
+            {
+                double average = 0;
+
+                for (int i = 0; i < treasure.Count; i++)
+                {
+                    average += treasure[i].Length;
+                }
+
+                average /= treasure.Count;
+
+                Console.WriteLine($"Average treasure gain: {average:f2} pirate credits.");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals pt2/ExamPreparation/03. Memory Game/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Memory_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine().Split(' ').ToList();
+
+            string command = Console.ReadLine();
+            int moves = 0;
+
+            while (command != "end")
+            {
+                string[] tokens = command.Split();
+                int num1 = int.Parse(tokens[0]);
+                int num2 = int.Parse(tokens[1]);
+
+                if (num1 == num2 && num1 < 0 && num2 < 0 && num1 > numbers.Count && num2 > numbers.Count)
+                {
+                    moves++;
+
+                    numbers.Insert(numbers.Count / 2, $"-{moves}a");
+
+                }
+            }
+        }
+    }
+}
