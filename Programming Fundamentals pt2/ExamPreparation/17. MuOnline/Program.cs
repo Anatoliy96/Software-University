3cb6daf33bba1c353c7ea6df9156a00b0eb1d756
Anatoliy <leaved@abv.bg>
2023-02-18 18:23:05 +0200

    Exam Preparation

---
+++ Programming Fundamentals pt2/ExamPreparation/17. MuOnline/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+namespace _17._MuOnline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] roomsInDungeons = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            int currentHealth = 100;
+            int bitcoins = 0;
+            bool isDead = false;
+            int roomsCount = 0;
+            int damageTaken = 0;
+
+            foreach (var room in roomsInDungeons)
+            {
+                string[] parts = room.Split();
+                string command = parts[0];
+                int amount = int.Parse(parts[1]);
+
+                roomsCount++;
+
+                if (command == "potion")
+                {
+                    currentHealth += amount;
+
+                    if (currentHealth > 100)
+                    {
+                        currentHealth = 100;
+                        Console.WriteLine($"You healed for {damageTaken} hp.");
+                        Console.WriteLine($"Current health: {currentHealth} hp.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You healed for {amount} hp.");
+                        Console.WriteLine($"Current health: {currentHealth} hp.");
+                    }
+                    
+                }
+                else if (command == "chest")
+                {
+                    bitcoins += amount;
+
+                    Console.WriteLine($"You found {amount} bitcoins.");
+                    
+                }
+                else
+                {
+                    currentHealth -= amount;
+                    damageTaken = amount;
+
+                    if (currentHealth <= 0)
+                    {
+                        isDead = true;
+                        Console.WriteLine($"You died! Killed by {command}.");
+                        Console.WriteLine($"Best room: {roomsCount}");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You slayed {command}.");
+                    }
+                }
+            }
+
+            if (!isDead)
+            {
+                Console.WriteLine("You've made it!");
+                Console.WriteLine($"Bitcoins: {bitcoins}");
+                Console.WriteLine($"Health: {currentHealth}");
+            }
+
+        }
+    }
+}
---
+++ Programming Fundamentals pt2/ExamPreparation/18. Inventory/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _18._Inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inventory = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            string[] tokens = Console.ReadLine().Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (tokens[0] != "Craft!")
+            {
+                string command = tokens[0];
+                string item = tokens[1];
+
+                if (command == "Collect")
+                {
+                    if (!inventory.Contains(item))
+                    {
+                        inventory.Add(item);
+                    }
+                }
+                else if (tokens[0] == "Drop")
+                {
+                    if (inventory.Contains(item))
+                    {
+                        inventory.Remove(item);
+                    }
+                }
+                else if (tokens[0] == "Combine Items")
+                {
+                    string[] combineItems = item.Split(":");
+                    string oldItem = combineItems[0];
+                    string newItem = combineItems[1];
+
+                    int index = inventory.IndexOf(oldItem);
+
+                    if (index != -1)
+                    {
+                        inventory.Insert(index + 1, newItem);
+                    }
+                }
+                else if (tokens[0] == "Renew")
+                {
+                    if (inventory.Contains(item))
+                    {
+                        inventory.Remove(item);
+                        inventory.Add(item);
+                    }
+                }
+                tokens = Console.ReadLine().Split(" - ",StringSplitOptions.RemoveEmptyEntries);
+            }
+            Console.WriteLine(string.Join(", ", inventory));
+        }
+    }
+}
