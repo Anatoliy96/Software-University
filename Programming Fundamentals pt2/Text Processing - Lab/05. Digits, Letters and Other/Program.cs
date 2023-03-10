23d7a4347df237a40c1d5963015cd7fa746ea835
Anatoliy <leaved@abv.bg>
2023-03-10 19:12:37 +0200

    Text processing - Lab

---
+++ Programming Fundamentals pt2/Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder digits = new StringBuilder();
+            StringBuilder letter = new StringBuilder();
+            StringBuilder chars = new StringBuilder();
+
+            foreach (var ch in text)
+            {
+                if (char.IsDigit(ch))
+                {
+                    digits.Append(ch);
+                }
+                else if (char.IsLetter(ch))
+                {
+                    letter.Append(ch);
+                }
+                else
+                {
+                    chars.Append(ch);
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letter);
+            Console.WriteLine(chars);
+        }
+    }
+}
