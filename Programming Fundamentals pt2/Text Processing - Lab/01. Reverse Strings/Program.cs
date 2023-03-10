8de7b802c6dd306804fcb81440f31b3084b6469f
Anatoliy <leaved@abv.bg>
2023-03-10 11:29:58 +0200

    Text Processing - Lab

---
+++ Programming Fundamentals pt2/Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string reversed = string.Empty;
+
+            while (input != "end")
+            {
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    reversed += input[i];
+                }
+
+                Console.WriteLine($"{input} = {reversed}");
+
+                reversed = string.Empty;
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
