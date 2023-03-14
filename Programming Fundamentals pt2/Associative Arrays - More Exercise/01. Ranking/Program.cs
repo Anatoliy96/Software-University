b366cf6261b30311740eaa51c98d458631e2d1be
Anatoliy <leaved@abv.bg>
2023-03-14 21:02:58 +0200

    Associative Arrays - More Exercise

---
+++ Programming Fundamentals pt2/Associative Arrays - More Exercise/01. Ranking/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Ranking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Contest> contests = new Dictionary<string, Contest>();
+            Dictionary<string, User> users = new Dictionary<string, User>();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end of contests")
+            {
+                string[] courses = command.Split(":");
+
+                string course = courses[0];
+                string password = courses[1];
+
+                contests.Add(course, new Contest
+                {
+                    Password = password,
+                    Participants = new Dictionary<string, int>()
+                });
+            }
+
+            while ((command = Console.ReadLine()) != "end of submissions")
+            {
+                string[] submissions = command.Split("=>");
+
+                string course = submissions[0];
+                string password = submissions[1];
+                string username = submissions[2];
+                int score = int.Parse(submissions[3]);
+
+                if (contests.ContainsKey(course) && contests[course].Password == password)
+                {
+                    if (!users.ContainsKey(username))
+                    {
+                        users.Add(username, new User
+                        {
+                            Name = username,
+                            PointsByContest = new Dictionary<string, int>()
+                        });
+                    }
+                }
+                if (!users[username].PointsByContest.ContainsKey(course))
+                {
+                    users[username].PointsByContest.Add(course, score);
+                }
+                else if (score > users[username].PointsByContest[course])
+                {
+                    users[username].PointsByContest[course] = score;
+                }
+
+                User bestCandidate = users.Values.OrderByDescending(u => u.TotalPoints).FirstOrDefault();
+                Console.WriteLine($"Best candidate is {bestCandidate.Name} with total {bestCandidate.TotalPoints} points.");
+
+                foreach (User user in users.Values.OrderBy(u => u.Name))
+                {
+                    Console.WriteLine(user.Name);
+                    foreach (KeyValuePair<string, int> contestPoints in user.PointsByContest.OrderByDescending(cp => cp.Value))
+                    {
+                        Console.WriteLine($"# {contestPoints.Key} -> {contestPoints.Value}");
+                    }
+                }
+            }
+        }
+
+        public class Contest
+        {
+            public string Password { get; set; }
+            public Dictionary<string, int> Participants { get; set; }
+        }
+        public class User
+        {
+            public string Name { get; set; }
+            public Dictionary<string, int> PointsByContest { get; set; }
+
+            public int TotalPoints => PointsByContest.Values.Sum();
+        }
+    }
+}
---
+++ Programming Fundamentals pt2/Associative Arrays - More Exercise/05. Dragon Army/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Dragon_Army
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<Dragon>> dragons = new Dictionary<string, List<Dragon>>();
+            
+            int defaultHealth = 250;
+            int defaultDamage = 45;
+            int defaultArmor = 10;
+
+            double averageDamage = 0;
+            double averageArmor = 0;
+            double averageHealth = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(' ');
+
+                string type = command[0];
+                string name = command[1];
+                string damage = command[2];
+                string health = command[3];
+                string armor = command[4];
+
+                if (damage == "null")
+                {
+                    damage = defaultDamage.ToString();
+                    defaultDamage = int.Parse(damage);
+                }
+                if (health == "null")
+                {
+                    health = defaultHealth.ToString();
+                    defaultHealth = int.Parse(health);
+                }
+                if (armor == "null")
+                {
+                    armor = defaultArmor.ToString();
+                    defaultArmor = int.Parse(armor);
+                }
+
+                Dragon dragon = new Dragon();
+
+                dragon.Name = name;
+                dragon.Damage = int.Parse(damage);
+                dragon.Health = int.Parse(health);
+                dragon.Armor = int.Parse(armor);
+
+                foreach (var kvp in dragons)
+                {
+                    foreach (var drg in kvp.Value)
+                    {
+                        if (kvp.Key == type && drg.Name == name)
+                        {
+                            drg.Damage = int.Parse(damage);
+                            drg.Health = int.Parse(health);
+                            drg.Armor = int.Parse(armor);
+
+                            continue;
+                        }
+                    }
+                }
+
+                if (!dragons.ContainsKey(type))
+                {
+                    dragons.Add(type, new List<Dragon>());
+                }
+
+                dragons[type].Add(dragon);
+            }
+
+            foreach (var dragon in dragons)
+            {
+                foreach (var drg in dragon.Value)
+                {
+                    averageDamage += drg.Damage;
+                    averageArmor += drg.Armor;
+                    averageHealth += drg.Health;
+                }
+
+                averageDamage /= dragon.Value.Count;
+                averageHealth /= dragon.Value.Count;
+                averageArmor /= dragon.Value.Count;
+
+                Console.WriteLine($"{dragon.Key}::({averageDamage:f2}/{averageHealth:f2}/{averageArmor:f2})");
+
+                foreach (var drg in dragon.Value.OrderBy(d => d.Name))
+                {
+                    Console.WriteLine($"-{drg.Name} -> damage: {drg.Damage}, health: {drg.Health}, armor: {drg.Armor}");
+                }
+
+                averageDamage = 0;
+                averageHealth = 0;
+                averageArmor = 0;
+            }
+        }
+
+        public class Dragon
+        {
+            public string Name { get; set; }
+            public int Damage { get; set; }
+            public int Health { get; set; }
+            public int Armor { get; set; }
+        }
+    }
+}
