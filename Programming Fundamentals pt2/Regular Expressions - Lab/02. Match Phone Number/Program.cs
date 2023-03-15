7d47949ad8e6870af05daca32a04769259319c76
Anatoliy <leaved@abv.bg>
2023-03-15 21:24:09 +0200

    Regular Expressions - Lab

---
+++ Programming Fundamentals pt2/Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"\+359( |-)2\1\d{3}\1\d{4}\b";
+
+            MatchCollection result = Regex.Matches(input, pattern);
+
+            Console.WriteLine(String.Join(", ", result));
+        }
+    }
+}
---
+++ Programming Fundamentals pt2/Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(?<day>\d{2})(.|-|/)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection result = regex.Matches(input);
+
+
+            foreach (Match match in result)
+            {
+                var mathchGroups = match.Groups;
+                Console.WriteLine($"Day: {mathchGroups["day"]}, Month: {mathchGroups["month"]}, Year: {mathchGroups["year"]}");
+            }
+        }
+    }
+}
