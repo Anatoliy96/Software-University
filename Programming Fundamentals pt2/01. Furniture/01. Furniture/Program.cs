bb363494f4a385a808010942726b3de283850e03
Anatoliy <leaved@abv.bg>
2023-03-22 15:58:40 +0200

    Regular Expressions - Exercise

---
+++ Programming Fundamentals pt2/01. Furniture/01. Furniture/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            Dictionary<string, decimal> furnitures = new Dictionary<string, decimal>();
+
+            while ((input = Console.ReadLine()) != "Purchase")
+            {
+                string pattern = @">>(?<name>[A-Z]+[a-z]*( )*[a-z]*)<<(?<price>\d+(.\d+)?)!(?<quantity>\d+)";
+
+                Regex regex = new Regex(pattern);
+
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    string name = match.Groups["name"].Value;
+                    decimal price = decimal.Parse(match.Groups["price"].Value);
+                    int quantity = int.Parse(match.Groups["quantity"].Value);
+
+                    if (!furnitures.ContainsKey(name))
+                    {
+                        furnitures[name] = 0;
+                    }
+
+                    furnitures[name] += price * quantity;
+                }
+            }
+
+            Console.WriteLine("Bought furniture:");
+            foreach (string name in furnitures.Keys)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("Total money spend: {0:f2}", furnitures.Values.Sum());
+        }
+    }
+}
---
+++ Programming Fundamentals pt2/01. Furniture/02. Race/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine().Split(",");
+            string text = Console.ReadLine();
+
+            Dictionary<string, int> people = new Dictionary<string, int>();
+
+            int sum = 0;
+            string result = string.Empty;
+
+            while (text != "end of race")
+            {
+                string digits = @"(?<digits>[\d+])";
+                string letters = @"(?<letters>[A-Za-z+])";
+
+                Regex lettersRegex = new Regex(letters);
+                Regex digitsRegex = new Regex(digits);
+
+                MatchCollection letter = lettersRegex.Matches(text);
+                MatchCollection digit = digitsRegex.Matches(text);
+                
+                for (int i = 0; i < letter.Count; i++)
+                {
+                    result += letter[i];
+                }
+
+                foreach (Match match in digit)
+                {
+                    sum += int.Parse(match.Groups["digits"].Value);
+                }
+
+                foreach (var name in command)
+                {
+                    if (name.Contains(result))
+                    {
+                        if (!people.ContainsKey(name))
+                        {
+                            people.Add(name, sum);
+                        }
+                        else
+                        {
+                            people[name] += sum;
+                        }
+                    }
+                }
+
+                result = string.Empty;
+                sum = 0;
+
+                text = Console.ReadLine();
+            }
+
+            var sortedDictonary = people.OrderByDescending(x => x.Value).Take(3);
+
+            Console.WriteLine("1st place: {0}",sortedDictonary.ElementAt(0).Key);
+            Console.WriteLine("2nd place:{0}",sortedDictonary.ElementAt(1).Key);
+            Console.WriteLine("3rd place:{0}",sortedDictonary.ElementAt(2).Key);
+
+        }
+    }
+}
