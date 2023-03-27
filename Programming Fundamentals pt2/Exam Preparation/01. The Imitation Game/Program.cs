05dc04fb4693854274633875c2de48ca35b1c216
Anatoliy <leaved@abv.bg>
2023-03-27 15:53:11 +0300

    Exam Preparation 1.

---
+++ Programming Fundamentals pt2/Exam Preparation/01. The Imitation Game/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _01._The_Imitation_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedMessage = Console.ReadLine();
+            string[] commands = Console.ReadLine().Split("|");
+            string letters = string.Empty;
+            
+            while (commands[0] != "Decode")
+            {
+                if (commands[0] == "Move")
+                {
+                    int numberOfLetters = int.Parse(commands[1]);
+
+                    letters = encryptedMessage.Substring(0, numberOfLetters);
+                    encryptedMessage = encryptedMessage.Remove(0, numberOfLetters);
+                    encryptedMessage = encryptedMessage.Insert(encryptedMessage.Length, letters);
+                }
+                else if (commands[0] == "Insert")
+                {
+                    int index = int.Parse(commands[1]);
+                    string value = commands[2];
+
+                    encryptedMessage = encryptedMessage.Insert(index, value);
+                }
+                else if (commands[0] == "ChangeAll")
+                {
+                    string subString = commands[1];
+                    string replacement = commands[2];
+
+                    foreach (var occurrence in encryptedMessage)
+                    {
+                        if (occurrence.ToString() == subString)
+                        {
+                            encryptedMessage = encryptedMessage.Replace(subString, replacement);
+                        }
+                    }
+                }
+
+                commands = Console.ReadLine().Split("|");
+            }
+
+            Console.WriteLine($"The decrypted message is: {encryptedMessage}");
+        }
+    }
+}
---
+++ Programming Fundamentals pt2/Exam Preparation/03. Need for Speed III/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Need_for_Speed_III
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Car> cars = new Dictionary<string, Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split("|");
+
+                string car = input[0];
+                int mileAge = int.Parse(input[1]);
+                int fuel = int.Parse(input[2]);
+
+                if (!cars.ContainsKey(car))
+                {
+                    cars.Add(car, new Car());
+
+                    cars[car].MileAge = mileAge;
+                    cars[car].Fuel = fuel;
+
+                }
+                //cars[car].Add(mileAge, fuel);
+            }
+
+            string[] command = Console.ReadLine().Split(" : ");
+
+            while (command[0] != "Stop")
+            {
+                if (command[0] == "Drive")
+                {
+                    string car = command[1];
+                    int distance = int.Parse(command[2]);
+                    int fuel = int.Parse(command[3]);
+
+                    if (cars.ContainsKey(car))
+                    {
+                        if (cars[car].Fuel >= fuel)
+                        {
+                            cars[car].MileAge += distance;
+                            cars[car].Fuel -= fuel;
+
+                            Console.WriteLine($"{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not enough fuel to make that ride");
+                        }
+                        if (cars[car].MileAge >= 100000)
+                        {
+                            Console.WriteLine($"Time to sell the {car}!");
+                            cars.Remove(car);
+
+                        }
+                    }
+                }
+                else if (command[0] == "Refuel")
+                {
+                    string car = command[1];
+                    int fuel = int.Parse (command[2]);
+
+                    if (cars.ContainsKey(car))
+                    {
+                        int tankCapacity = 75;
+                        int refuel = tankCapacity - cars[car].Fuel;
+
+                        cars[car].Fuel += fuel;
+
+                        if (cars[car].Fuel > tankCapacity)
+                        {
+                            Console.WriteLine($"{car} refueled with {refuel} liters");
+                            cars[car].Fuel = tankCapacity;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{car} refueled with {fuel} liters");
+                        }
+                    }
+                }
+                else if (command[0] == "Revert")
+                {
+                    string car = command[1];
+                    int kilometers = int.Parse(command[2]);
+
+                    if (cars.ContainsKey(car))
+                    {
+                        cars[car].MileAge -= kilometers;
+                        if (cars[car].MileAge < 10000)
+                        {
+                            cars[car].MileAge = 10000;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{car} mileage decreased by {kilometers} kilometers");
+                        }
+                    }
+                }
+                command = Console.ReadLine().Split(" : ");
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Key} -> Mileage: {car.Value.MileAge} kms, Fuel in the tank: {car.Value.Fuel} lt.");
+            }
+        }
+
+        public class Car
+        {
+            public int MileAge { get; set; }
+            public int Fuel { get; set; }
+        }
+    }
+}
