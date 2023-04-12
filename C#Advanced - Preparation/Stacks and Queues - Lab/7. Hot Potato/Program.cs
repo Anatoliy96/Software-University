eafe63d532e541296837780575d6fcf920f32a48
Anatoliy <leaved@abv.bg>
2023-04-13 00:22:42 +0300

    7.Hot Potato

---
+++ C#Advanced - Preparation/Stacks and Queues - Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> queue = new Queue<string>();
+
+            foreach (var item in input)
+            {
+                queue.Enqueue(item);
+            }
+
+            int turn = 1;
+
+            while (queue.Count > 1)
+            {
+                string currentKid = queue.Dequeue();
+
+                if (turn == n)
+                {
+                    Console.WriteLine($"Removed {currentKid}");
+                    turn = 1;
+                    continue;
+                }
+
+                queue.Enqueue(currentKid);
+                turn++;
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
