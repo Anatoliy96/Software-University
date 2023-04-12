00fe4660099393bd6555deb1f28f8b48c7e12db7
Anatoliy Dudiev <anatoliyd@abv.bg>
2023-04-12 15:38:58 +0300

    Add files via upload

---
+++ C# Advanced-Preparation/Stacks and Queues/3. Simple Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split(' ');
+
+            Stack<string> stack = new Stack<string>();
+            int result = 0;
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                stack.Push(tokens[i]);
+
+                if (stack.Count == 3)
+                {
+                    int firstNumber = int.Parse(stack.Pop());
+                    var operatation = stack.Pop();
+                    int secondNumber = int.Parse(stack.Pop());
+
+                    if (operatation == "+")
+                    {
+                        result = firstNumber + secondNumber;
+                    }
+                    else if (operatation == "-")
+                    {
+                        result = secondNumber - firstNumber;
+                    }
+
+                    stack.Push(result.ToString());
+                }
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
