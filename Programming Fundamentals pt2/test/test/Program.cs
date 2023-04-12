7ce55e785e19fdee5b24b1e780f49b517318c31d
Anatoliy <leaved@abv.bg>
2023-04-12 14:43:27 +0300

    1.Reverse string

---
+++ Programming Fundamentals pt2/test/test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Text("c#");
+        }
+        public static void Text(string text)
+        {
+            Console.WriteLine("i love" + text);
+        }
+    }
+}
