01876234d6116e4cc0933a2500e308b6250d1ae9
Anatoliy <leaved@abv.bg>
2023-04-13 19:02:13 +0300

    4.Fast Food

---
+++ C#Advanced - Preparation/Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+
+            int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> clients = new Queue<int>();
+
+            int maxOrder = int.MinValue;
+
+            foreach (var order in orders)
+            {
+                clients.Enqueue(order);
+            }
+
+            foreach (var order in clients)
+            {
+                if (order > maxOrder)
+                {
+                    maxOrder = order;
+                }
+            }
+            Console.WriteLine(maxOrder);
+
+            while (clients.Count > 0)
+            {
+                if (foodQuantity >= clients.Peek())
+                {
+                    foodQuantity -= clients.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", clients)}");
+                    return;
+                }
+            }
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
