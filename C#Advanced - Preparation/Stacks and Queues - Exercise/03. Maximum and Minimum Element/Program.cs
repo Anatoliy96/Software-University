044b7fc42e3b9c8d685195562ff4cbc01989c3a2
Anatoliy <leaved@abv.bg>
2023-04-13 18:22:13 +0300

    03. Maximum and Minimum Element

---
+++ C#Advanced - Preparation/Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Maximum_and_Minimum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> numbers = new Stack<int>();
+
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] command = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                if (command[0] == 1)
+                {
+                    int numToPush = command[1];
+
+                    numbers.Push(numToPush);
+                }
+                else if (command[0] == 2)
+                {
+                    numbers.Pop();
+                }
+                else if (command[0] == 3)
+                {
+                    if (numbers.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    foreach (var item in numbers)
+                    {
+                        if (item > max)
+                        {
+                            max = item;
+                        }
+                    }
+                    Console.WriteLine(max);
+                }
+                else if (command[0] == 4)
+                {
+                    if (numbers.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    foreach (var item in numbers)
+                    {
+                        if (item < min)
+                        {
+                            min = item;
+                        }
+                    }
+                    Console.WriteLine(min);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+    }
+}
