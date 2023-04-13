b1267a8b8ecef12da5d2647db15a6bfb5a5b1722
Anatoliy <leaved@abv.bg>
2023-04-13 18:06:06 +0300

    01. Basic Stack Operations 02. Basic Queue Operations

---
+++ C#Advanced - Preparation/Stacks and Queues - Exercise/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] command = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] elements = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Stack<int> numbers = new Stack<int>();
+
+            int n = command[0];
+            int s = command[1];
+            int x = command[2];
+
+            int min = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Push(elements[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                numbers.Pop();
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            foreach (var item in numbers)
+            {
+                if (item == x)
+                {
+                    Console.WriteLine("true");
+                    return;
+                }
+            }
+
+            foreach (var item in numbers)
+            {
+                if (item < min)
+                {
+                    min = item;
+                    Console.WriteLine(min);
+                }
+            }
+        }
+    }
+}
---
+++ C#Advanced - Preparation/Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] command = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] elements = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Queue<int> numbers = new Queue<int>();
+
+            int n = command[0];
+            int s = command[1];
+            int x = command[2];
+
+            int min = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Enqueue(elements[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                numbers.Dequeue();
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            foreach (var item in numbers)
+            {
+                if (item == x)
+                {
+                    Console.WriteLine("true");
+                    return;
+                }
+            }
+
+            foreach (var item in numbers)
+            {
+                if (item < min)
+                {
+                    min = item;
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
