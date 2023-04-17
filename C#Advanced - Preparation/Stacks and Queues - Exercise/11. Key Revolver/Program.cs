9d4424f332cf1601ed873895157abf439e8c4bc0
Anatoliy <leaved@abv.bg>
2023-04-17 11:22:47 +0300

    11. Key Revolver

---
+++ C#Advanced - Preparation/Stacks and Queues - Exercise/11. Key Revolver/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._Key_Revolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int sizeOfGunBarrel = int.Parse(Console.ReadLine());
+            int[] bullets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] locks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int intelligenceValue = int.Parse(Console.ReadLine());
+
+            int bulletsCount = 0;
+            int barrel = 0;
+
+            Queue<int> treasureLocks = new Queue<int>(locks);
+            Stack<int> revolverBullets = new Stack<int>(bullets);
+
+            while (treasureLocks.Count > 0 && revolverBullets.Count > 0)
+            {
+                if (revolverBullets.Peek() <= treasureLocks.Peek())
+                {
+                    bulletsCount++;
+                    barrel++;
+                    Console.WriteLine("Bang!");
+                    revolverBullets.Pop();
+                    treasureLocks.Dequeue();
+                }
+                else
+                {
+                    bulletsCount++;
+                    barrel++;
+                    Console.WriteLine("Ping!");
+                    revolverBullets.Pop();
+                }
+                if (barrel == sizeOfGunBarrel)
+                {
+                    if (revolverBullets.Count > 0)
+                    {
+                        Console.WriteLine("Reloading!");
+                        barrel = 0;
+                    }
+                }
+            }
+
+            if (revolverBullets.Count == 0 && treasureLocks.Count > 0)
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {treasureLocks.Count}");
+            }
+            else if (revolverBullets.Count >= 0 && treasureLocks.Count == 0)
+            {
+                int bulletCost = bulletsCount * bulletPrice;
+                int moneyEarned = intelligenceValue - bulletCost;
+
+                Console.WriteLine($"{revolverBullets.Count} bullets left. Earned ${moneyEarned}");
+            }
+        }
+    }
+}
