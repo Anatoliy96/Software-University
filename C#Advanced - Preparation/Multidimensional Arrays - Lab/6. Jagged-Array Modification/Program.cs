a960f25a4d5dc5486c11201b259d04751fa6225c
Anatoliy <leaved@abv.bg>
2023-04-18 14:14:44 +0300

    6. Jagged-Array Modification

---
+++ C#Advanced - Preparation/Multidimensional Arrays - Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] jaggedArray = new int[n][];
+
+            for (int row = 0; row < jaggedArray.Length; row++)
+            {
+                int[] values = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                jaggedArray[row] = new int[values.Length];
+
+                for (int col = 0; col < jaggedArray[row].Length; col++)
+                {
+                    jaggedArray[row][col] = values[col];
+                }
+            }
+
+            string[] command = Console.ReadLine().Split();
+
+            while (command[0] != "END")
+            {
+                if (command[0] == "Add")
+                {
+                    int row = int.Parse(command[1]);
+                    int col = int.Parse(command[2]);
+                    int value = int.Parse(command[3]);
+
+                    if (row >= 0 && row < jaggedArray[row].Length && col >= 0 && col < jaggedArray[row].Length)
+                    {
+                        jaggedArray[row][col] += value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                }
+                else if (command[0] == "Subtract")
+                {
+                    int row = int.Parse(command[1]);
+                    int col = int.Parse(command[2]);
+                    int value = int.Parse(command[3]);
+
+                    if (row >= 0 && row < jaggedArray[row].Length && col >= 0 && col < jaggedArray[row].Length)
+                    {
+                        jaggedArray[row][col] -= value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                }
+                command = Console.ReadLine().Split();
+            }
+
+            for (int row = 0; row < jaggedArray.Length; row++)
+            {
+                for (int col = 0; col < jaggedArray[row].Length; col++)
+                {
+                    Console.Write(jaggedArray[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
