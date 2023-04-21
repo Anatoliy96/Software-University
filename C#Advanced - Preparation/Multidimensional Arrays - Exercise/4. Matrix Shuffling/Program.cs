36c0152a89416e5759376e2292a4e86e8d36551a
Anatoliy <leaved@abv.bg>
2023-04-21 13:48:46 +0300

    4. Matrix Shuffling

---
+++ C#Advanced - Preparation/Multidimensional Arrays - Exercise/4. Matrix Shuffling/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Matrix_Shuffling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            string[,] matrix = new string[sizes[0], sizes[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] cols = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = cols[col];
+                }
+            }
+
+            string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "END")
+            {
+                if (IsValid(sizes, command))
+                {
+                    string temp = matrix[int.Parse(command[1]), int.Parse(command[2])];
+                    matrix[int.Parse(command[1]), int.Parse(command[2])] = matrix[int.Parse(command[3]), int.Parse(command[4])];
+                    matrix[int.Parse(command[3]), int.Parse(command[4])] = temp;
+
+                    for (int row = 0; row < matrix.GetLength(0); row++)
+                    {
+                        for (int col = 0; col < matrix.GetLength(1); col++)
+                        {
+                            Console.Write(matrix[row, col] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+                command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+
+        static bool IsValid(int[] sizes, string[] command)
+        {
+            return
+                command[0] == "swap" && command.Length == 5
+                && int.Parse(command[1]) >= 0 && int.Parse(command[1]) < sizes[0]
+                && int.Parse(command[2]) >= 0 && int.Parse(command[2]) < sizes[1]
+                && int.Parse(command[3]) >= 0 && int.Parse(command[3]) < sizes[0]
+                && int.Parse(command[4]) >= 0 && int.Parse(command[4]) < sizes[1];
+        }
+    }
+}
