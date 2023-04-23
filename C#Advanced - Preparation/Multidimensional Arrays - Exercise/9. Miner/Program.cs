2785e49e194c54989632a675592ef5f6830cd609
Anatoliy <leaved@abv.bg>
2023-04-23 16:32:53 +0300

    9. Miner - finished

--- C#Advanced - Preparation/Multidimensional Arrays - Exercise/9. Miner/Program.cs
+++ C#Advanced - Preparation/Multidimensional Arrays - Exercise/9. Miner/Program.cs
@@ -47,9 +47,115 @@ namespace _9._Miner
             {
                 if (commands[i] == "up")
                 {
-
+                    if (IsExist(rowStartPostition - 1, colStartPostition, matrix))
+                    {
+                        if (matrix[rowStartPostition - 1, colStartPostition] == "*")
+                        {
+                            matrix[rowStartPostition - 1, colStartPostition] = "s";
+                            matrix[rowStartPostition, colStartPostition] = "*";
+                            rowStartPostition = rowStartPostition - 1;
+                        }
+                        else if (matrix[rowStartPostition - 1, colStartPostition] == "c")
+                        {
+                            matrix[rowStartPostition - 1, colStartPostition] = "s";
+                            matrix[rowStartPostition, colStartPostition] = "*";
+                            rowStartPostition = rowStartPostition - 1;
+                            coalCount--;
+                        }
+                        else if (matrix[rowStartPostition - 1, colStartPostition] == "e")
+                        {
+                            rowStartPostition = rowStartPostition - 1;
+                            Console.WriteLine($"Game over! ({rowStartPostition}, {colStartPostition})");
+                            return;
+                        }
+                    }
+                }
+                else if (commands[i] == "right")
+                {
+                    if (IsExist(rowStartPostition, colStartPostition + 1, matrix))
+                    {
+                        if (matrix[rowStartPostition, colStartPostition + 1] == "*")
+                        {
+                            matrix[rowStartPostition, colStartPostition + 1] = "s";
+                            matrix[rowStartPostition, colStartPostition] = "*";
+                            colStartPostition = colStartPostition + 1;
+                        }
+                        else if (matrix[rowStartPostition, colStartPostition + 1] == "c")
+                        {
+                            matrix[rowStartPostition, colStartPostition + 1] = "s";
+                            matrix[rowStartPostition, colStartPostition] = "*";
+                            colStartPostition = colStartPostition + 1;
+                            coalCount--;
+                        }
+                        else if (matrix[rowStartPostition, colStartPostition + 1] == "e")
+                        {
+                            colStartPostition = colStartPostition + 1;
+                            Console.WriteLine($"Game over! ({rowStartPostition}, {colStartPostition})");
+                            return;
+                        }
+                    }
+                }
+                else if (commands[i] == "left")
+                {
+                    if (IsExist(rowStartPostition, colStartPostition - 1, matrix))
+                    {
+                        if (matrix[rowStartPostition, colStartPostition - 1] == "*")
+                        {
+                            matrix[rowStartPostition, colStartPostition - 1] = "s";
+                            matrix[rowStartPostition, colStartPostition] = "*";
+                            colStartPostition = colStartPostition - 1;
+                        }
+                        else if (matrix[rowStartPostition, colStartPostition - 1] == "c")
+                        {
+                            matrix[rowStartPostition, colStartPostition - 1] = "s";
+                            matrix[rowStartPostition, colStartPostition] = "*";
+                            colStartPostition = colStartPostition - 1;
+                            coalCount--;
+                        }
+                        else if (matrix[rowStartPostition, colStartPostition - 1] == "e")
+                        {
+                            colStartPostition = colStartPostition - 1;
+                            Console.WriteLine($"Game over! ({rowStartPostition}, {colStartPostition})");
+                            return;
+                        }
+                    }
+                }
+                else if (commands[i] == "down")
+                {
+                    if (IsExist(rowStartPostition + 1, colStartPostition, matrix))
+                    {
+                        if (matrix[rowStartPostition + 1, colStartPostition] == "*")
+                        {
+                            matrix[rowStartPostition + 1, colStartPostition] = "s";
+                            matrix[rowStartPostition, colStartPostition] = "*";
+                            rowStartPostition = rowStartPostition + 1;
+                        }
+                        else if (matrix[rowStartPostition + 1, colStartPostition] == "c")
+                        {
+                            matrix[rowStartPostition + 1, colStartPostition] = "s";
+                            matrix[rowStartPostition, colStartPostition] = "*";
+                            rowStartPostition = rowStartPostition + 1;
+                            coalCount--;
+                        }
+                        else if (matrix[rowStartPostition + 1, colStartPostition] == "e")
+                        {
+                            rowStartPostition = rowStartPostition + 1;
+                            Console.WriteLine($"Game over! ({rowStartPostition}, {colStartPostition})");
+                            return;
+                        }
+                    }
+                }
+                if (coalCount == 0)
+                {
+                    Console.WriteLine($"You collected all coals! ({rowStartPostition}, {colStartPostition})");
+                    return;
                 }
             }
+            Console.WriteLine($"{coalCount} coals left. ({rowStartPostition}, {colStartPostition})");
+        }
+        static bool IsExist(int row, int col, string[,] matrix)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
         }
     }
 }
