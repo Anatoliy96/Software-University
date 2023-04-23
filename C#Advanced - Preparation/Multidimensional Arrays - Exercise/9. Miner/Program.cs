69c0ac8ce170f04d2a013cdca10536ef579cb2c8
Anatoliy <leaved@abv.bg>
2023-04-23 11:52:20 +0300

    9. Miner - not finished

---
+++ C#Advanced - Preparation/Multidimensional Arrays - Exercise/9. Miner/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _9._Miner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] commands = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string[,] matrix = new string[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] cols = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = cols[col];
+
+                }
+            }
+
+            int rowStartPostition = 0;
+            int colStartPostition = 0;
+            int coalCount = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == "s")
+                    {
+                        rowStartPostition = row;
+                        colStartPostition = col;
+                    }
+                    if (matrix[row, col] == "c")
+                    {
+                        coalCount++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < commands.Length; i++)
+            {
+                if (commands[i] == "up")
+                {
+
+                }
+            }
+        }
+    }
+}
