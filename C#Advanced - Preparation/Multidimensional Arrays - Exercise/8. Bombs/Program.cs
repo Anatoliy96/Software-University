aa4fdcaf967d3e61d79816562a0378e07d2c267c
Anatoliy <leaved@abv.bg>
2023-04-23 11:16:11 +0300

    8. Bombs - finished, now working

--- C#Advanced - Preparation/Multidimensional Arrays - Exercise/8. Bombs/Program.cs
+++ C#Advanced - Preparation/Multidimensional Arrays - Exercise/8. Bombs/Program.cs
@@ -7,7 +7,7 @@ namespace _8._Bombs
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());  
+            int n = int.Parse(Console.ReadLine());
 
             int[,] matrix = new int[n, n];
 
@@ -27,11 +27,74 @@ namespace _8._Bombs
                 int row1 = int.Parse(bomb.Split(",")[0]);
                 int col1 = int.Parse(bomb.Split(",")[1]);
 
-                if (matrix[row1, col1] > 0)
+                int value = matrix[row1, col1];
+
+                if (value > 0)
                 {
-                    
+                    if (IsExist(row1 - 1, col1 - 1, matrix) && matrix[row1 - 1, col1 - 1] > 0)
+                    {
+                        matrix[row1 - 1, col1 - 1] -= value;
+                    }
+                    if (IsExist(row1 - 1, col1, matrix) && matrix[row1 - 1, col1] > 0)
+                    {
+                        matrix[row1 - 1, col1] -= value;
+                    }
+                    if (IsExist(row1 - 1, col1 + 1, matrix) && matrix[row1 - 1, col1 + 1] > 0)
+                    {
+                        matrix[row1 - 1, col1 + 1] -= value;
+                    }
+                    if (IsExist(row1, col1 - 1, matrix) && matrix[row1, col1 - 1] > 0)
+                    {
+                        matrix[row1, col1 - 1] -= value;
+                    }
+                    if (IsExist(row1, col1 + 1, matrix) && matrix[row1, col1 + 1] > 0)
+                    {
+                        matrix[row1, col1 + 1] -= value;
+                    }
+                    if (IsExist(row1 + 1, col1 - 1, matrix) && matrix[row1 + 1, col1 - 1] > 0)
+                    {
+                        matrix[row1 + 1, col1 - 1] -= value;
+                    }
+                    if (IsExist(row1 + 1, col1, matrix) && matrix[row1 + 1, col1] > 0)
+                    {
+                        matrix[row1 + 1, col1] -= value;
+                    }
+                    if (IsExist(row1 + 1, col1 + 1, matrix) && matrix[row1 + 1, col1 + 1] > 0)
+                    {
+                        matrix[row1 + 1, col1 + 1] -= value;
+                    }
+
+                    matrix[row1, col1] = 0;
                 }
             }
+
+            int sum = 0;
+            int countOfCells = 0;
+
+            foreach (var cell in matrix)
+            {
+                if (cell > 0)
+                {
+                    sum += cell;
+                    countOfCells++;
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {countOfCells}");
+            Console.WriteLine($"Sum: {sum}");
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static bool IsExist(int row, int col, int[,] matrix)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
         }
     }
 }
