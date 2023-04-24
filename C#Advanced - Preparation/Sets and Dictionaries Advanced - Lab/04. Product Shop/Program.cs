bf47401a2609814a6fae1eb8a111d153482a0b2f
Anatoliy <leaved@abv.bg>
2023-04-24 17:24:42 +0300

    04. Product Shop

--- C#Advanced - Preparation/Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
+++ C#Advanced - Preparation/Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04._Product_Shop
 {
@@ -6,7 +8,47 @@ namespace _04._Product_Shop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string,double>>();
+
+            while (input[0] != "Revision")
+            {
+                string shop = input[0];
+                string product = input[1];
+                double price = double.Parse(input[2]);
+
+                if (!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+
+                    if (!shops[shop].ContainsKey(product))
+                    {
+                        shops[shop].Add(product, price);
+                    }
+                }
+                else
+                {
+                    if (!shops[shop].ContainsKey(product))
+                    {
+                        shops[shop].Add(product, price);
+                    }
+                }
+
+                input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            shops = shops.OrderBy(s => s.Key).ToDictionary(s => s.Key, s => s.Value);
+
+            foreach (var shop in shops)
+            {
+                Console.WriteLine($"{shop.Key}->");
+
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
         }
     }
 }
