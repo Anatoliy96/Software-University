d15de192d9687e49ab2f040c5e770376292f23c0
Anatoliy <leaved@abv.bg>
2023-04-24 15:09:37 +0300

    01. Count Same Values in Array

---
+++ C#Advanced - Preparation/Multidimensional Arrays - Exercise/10. Mutant Vampire Bunnies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10._Mutant_Vampire_Bunnies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ C#Advanced - Preparation/Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            Dictionary<double, int> times = new Dictionary<double, int>();
+
+            foreach (var num in numbers)
+            {
+                if (!times.ContainsKey(num))
+                {
+                    times.Add(num, 0);
+                }
+
+                times[num]++;
+            }
+
+            foreach (var item in times)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
