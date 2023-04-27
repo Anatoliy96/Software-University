b055c15ed66e9be730a11983fcb4ad2bc3fd3146
Anatoliy <leaved@abv.bg>
2023-04-27 16:28:32 +0300

    10. ForceBook

---
+++ C#Advanced - Preparation/Sets and Dictionaries Advanced - Exercise/10. ForceBook/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._ForceBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            Dictionary<string, string> sides = new Dictionary<string, string>();
+
+            while (command != "Lumpawaroo")
+            {
+                if (command.Contains("|"))
+                {
+                    string[] splited = command.Split(" | ");
+
+                    string side = splited[0];
+                    string name = splited[1];
+
+                    if (!sides.ContainsKey(name))
+                    {
+                        sides[name] = side;
+                    }
+                }
+                else
+                {
+                    string[] splited = command.Split(" -> ");
+                    string name = splited[0];
+                    string side = splited[1];
+
+                    if (sides.ContainsKey(name))
+                    {
+                        sides[name] = side;
+                        
+                    }
+                    else
+                    {
+                        sides[name] = side;
+                    }
+                    Console.WriteLine($"{name} joins the {side} side!");
+                }
+
+                command = Console.ReadLine();
+            }
+            var fillteredSideName = sides
+                .GroupBy(x => x.Value)
+                .OrderByDescending(x => x.Count())
+                .ThenBy(x => x.Key);
+
+            foreach (var kvp in fillteredSideName)
+            {
+                string side = kvp.Key;
+                var sideNameValue = kvp;
+
+                Console.WriteLine($"Side: {side}, Members: {sideNameValue.Count()}");
+
+                foreach (var kvpValue in sideNameValue.OrderBy(x => x.Key))
+                {
+                    string name = kvpValue.Key;
+                    string side2 = kvpValue.Value;
+
+                    Console.WriteLine($"! {name} ");
+                }
+            }
+        }
+    }
+}
