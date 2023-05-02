5047e2b78247bb069f65d95900dde1b0f9f95f29
Anatoliy <leaved@abv.bg>
2023-05-02 15:18:31 +0300

    04. Add VAT

---
+++ C#Advanced - Preparation/Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> VATprice = (p) => p * 1.20;
+
+            double[] prices = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(p => double.Parse(p))
+                .Select(p => VATprice(p))
+                .ToArray();
+
+            foreach (var price in prices)
+            {
+                Console.WriteLine($"{price:f2}");
+            }
+        }
+    }
+}
