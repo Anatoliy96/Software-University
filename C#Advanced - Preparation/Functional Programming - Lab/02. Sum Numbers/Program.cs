2c65888f6391b2fda49962b28639ba43cf273767
Anatoliy <leaved@abv.bg>
2023-05-02 15:00:54 +0300

    02. Sum Numbers

---
+++ C#Advanced - Preparation/Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            Console.WriteLine(input.Count());
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
