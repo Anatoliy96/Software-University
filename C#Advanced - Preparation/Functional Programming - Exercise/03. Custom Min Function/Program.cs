f5ab05c32f1c1e724f139594d25e7348ce2dbd94
Anatoliy <leaved@abv.bg>
2023-05-04 17:33:31 +0300

    03. Custom Min Function

---
+++ C#Advanced - Preparation/Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Func<int[], int> minFunc = arr =>
+            {
+                int min = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    if (min > arr[i])
+                    {
+                        min = arr[i];
+                    }
+                }
+                return min;
+            };
+
+            Console.WriteLine(minFunc(input));
+        }
+
+        
+    }
+}
