e6e138a104643dbf71eb435c44f41e8875c68f5d
Anatoliy <leaved@abv.bg>
2023-05-04 17:02:33 +0300

    01. Action Print 02. Knights of Honor

---
+++ C#Advanced - Preparation/Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(' ');
+
+            Action<string> action = PrintNames(names);
+        }
+
+        private static Action<string> PrintNames(string[] names)
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            return null;
+        }
+    }
+}
---
+++ C#Advanced - Preparation/Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(' ');
+
+            Action<string> action = (a) => Console.WriteLine("Sir" + " " + a);
+
+            names.ToList().ForEach(a => action(a));
+        }
+    }
+}
