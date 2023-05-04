e91c5d119f1bd60cb57ab959c8da5817ba663fba
Anatoliy <leaved@abv.bg>
2023-05-04 19:07:41 +0300

    05. Applied Arithmetics

---
+++ C#Advanced - Preparation/Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int[]> add = arr =>
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] += 1;
+                }
+                return arr;
+            };
+            Func<int[], int[]> multiply = arr =>
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] *= 2;
+                }
+
+                return arr;
+            };
+
+            Func<int[], int[]> subtract = arr =>
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] -= 1;
+                }
+
+                return arr;
+            };
+
+            Action<int[]> printer = arr =>
+            {
+                Console.WriteLine(String.Join(" ", arr));
+            };
+
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                if (command == "add")
+                {
+                    add(input);
+                }
+                else if (command == "multiply")
+                {
+                    multiply(input);
+                }
+                else if (command == "subtract")
+                {
+                    subtract(input);
+                }
+                else if (command == "print")
+                {
+                    printer(input);
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
