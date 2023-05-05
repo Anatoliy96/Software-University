00113bb1eeb6152d1489555263a3ef8a8c4103bc
Anatoliy <leaved@abv.bg>
2023-05-05 18:12:25 +0300

    09. Predicate Party!

---
+++ C#Advanced - Preparation/Functional Programming - Exercise/09. Predicate Party!/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Predicate_Party_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> people = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            
+            while (command[0] != "Party!")
+            {
+                string action = command[0];
+                string filter = command[1];
+                string value = command[2];
+
+                if (action == "Remove")
+                {
+                    people.RemoveAll(GetPredicate(filter, value));
+                }
+                else
+                {
+                    List<string> newPeople = people.FindAll(GetPredicate(filter, value));
+
+                    foreach (string person in newPeople)
+                    {
+                        int index = people.FindIndex(p => p == person);
+                        people.Insert(index, person);
+                    }
+                }
+                command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (people.Any())
+            {
+                Console.WriteLine($"{string.Join(", ", people)} are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+        }
+        static Predicate<string> GetPredicate(string filter, string value)
+        {
+            switch (filter)
+            {
+                case "StartsWith": 
+                    return p => p.StartsWith(value);
+                case "EndsWith":
+                    return p => p.EndsWith(value);
+                case "Length":
+                    return p => p.Length == int.Parse(value);
+                default:
+                    return default;
+                    
+            }
+        }
+    }
+}
