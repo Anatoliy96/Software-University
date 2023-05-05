edc24639e33cd1343cd1f96c81f4a13adc4c86b1
Anatoliy <leaved@abv.bg>
2023-05-05 18:31:53 +0300

    10. The Party Reservation Filter Module - not finished!

---
+++ C#Advanced - Preparation/Functional Programming - Exercise/10. ThePartyReservationFilterModule/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._ThePartyReservationFilterModule
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> people = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            string[] command = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "Print")
+            {
+                string action = command[0];
+                string filter = command[1];
+                string value = command[2];
+
+                command = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+
+            Console.WriteLine($"{string.Join(" ", people)}");
+
+
+        }
+        static Predicate<string> GetPredicate(string filter, string value)
+        {
+            switch (filter)
+            {
+                case "Starts with":
+                    return p => p.StartsWith(value);
+                case "Ends with":
+                    return p => p.EndsWith(value);
+                case "Length":
+                    return p => p.Length == int.Parse(value);
+                case "Contains":
+                    return p => p.Contains(value);
+                default:
+                    return default;
+
+            }
+        }
+    }
+}
+
