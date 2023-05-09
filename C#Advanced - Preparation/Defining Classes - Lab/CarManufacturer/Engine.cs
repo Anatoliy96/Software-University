57da52b5b64656ce3bafd2d3a4471ce6da31b9dc
Anatoliy <leaved@abv.bg>
2023-05-09 20:33:39 +0300

    04. Car Engine And Tires

---
+++ C#Advanced - Preparation/Defining Classes - Lab/CarManufacturer/Engine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        public Engine(int hourePower, double cubicCapacity)
+        {
+            this.HoursePower = hourePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+        
+        private int hoursePower;
+        private double cubicCapacity;
+
+        public int HoursePower { get { return hoursePower; } set { hoursePower = value; } }
+        public double CubicCapacity { get { return cubicCapacity; } set { cubicCapacity = value; } }
+    }
+}
--- C#Advanced - Preparation/Defining Classes - Lab/CarManufacturer/Program.cs
+++ C#Advanced - Preparation/Defining Classes - Lab/CarManufacturer/Program.cs
@@ -4,21 +4,35 @@
     {
         static void Main()
         {
-            string make = Console.ReadLine();
-            string model = Console.ReadLine();
-            int year = int.Parse(Console.ReadLine());
-            double fuelQuantity = double.Parse(Console.ReadLine());
-            double fuelConsumption = double.Parse(Console.ReadLine());
+            //string make = Console.ReadLine();
+            //string model = Console.ReadLine();
+            //int year = int.Parse(Console.ReadLine());
+            //double fuelQuantity = double.Parse(Console.ReadLine());
+            //double fuelConsumption = double.Parse(Console.ReadLine());
 
-            Car firstCar = new Car();
-            Car secondCar = new Car(make, model, year);
-            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+            //Car firstCar = new Car();
+            //Car secondCar = new Car(make, model, year);
+            //Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
 
-            Console.WriteLine(firstCar.WhoAmI());
+            //Console.WriteLine(firstCar.WhoAmI());
+            //Console.WriteLine();
+            //Console.WriteLine(secondCar.WhoAmI());
+            //Console.WriteLine();
+            //Console.WriteLine(thirdCar.WhoAmI());
+            //Console.WriteLine();
 
-            Console.WriteLine(secondCar.WhoAmI());
+            var tires = new Tire[4]
+            {
+                new Tire(2000, 1.25),
+                new Tire(2000, 1.25),
+                new Tire(2000, 1.25),
+                new Tire(2000, 1.25),
+            };
+            
+            var engine = new Engine(560, 6300);
 
-            Console.WriteLine(thirdCar.WhoAmI());
+            var car = new Car("Audi", "A3", 2001, 45, 7, engine, tires);
+            Console.WriteLine(car.WhoAmI());
         }
     }
 }
