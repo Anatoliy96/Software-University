5da2e2be71656546b7821e0ca402aaa9ba50fb26
Anatoliy <leaved@abv.bg>
2023-05-09 18:27:06 +0300

    01. Car

---
+++ C#Advanced - Preparation/Defining Classes - Lab/CarManufacturer/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            car.Make = "Audi";
+            car.Model = "A3";
+            car.Year = 2001;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
