68405a05926c43d5762d082eddc80bb8374efa24
Anatoliy <leaved@abv.bg>
2023-05-11 15:36:50 +0300

    3. Simple Calculator

---
+++ C# Advanced/Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+string[] tokens = Console.ReadLine().Split(' ');
+
+Stack<string> stack = new Stack<string>();
+
+int result = 0;
+
+for (int i = 0; i < tokens.Length; i++)
+{
+    stack.Push(tokens[i]);
+
+    if (stack.Count == 3)
+    {
+        int n1 = int.Parse(stack.Pop());
+        var operation = stack.Pop();
+        int n2 = int.Parse(stack.Pop());
+
+        if (operation == "+")
+        {
+            result = n1 + n2;
+        }
+        else if (operation == "-")
+        {
+            result = n2 - n1;
+        }
+
+        stack.Push(result.ToString());
+    }
+}
+Console.WriteLine(stack.Pop());
+
