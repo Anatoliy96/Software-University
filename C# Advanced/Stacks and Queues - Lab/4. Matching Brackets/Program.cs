bd3e7cf600b10dbcb66434a4dd4de8965c7a123c
Anatoliy <leaved@abv.bg>
2023-05-11 15:40:46 +0300

    4. Matching Brackets

---
+++ C# Advanced/Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+string input = Console.ReadLine();
+
+Stack<int> indexes = new Stack<int>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == '(')
+    {
+        indexes.Push(i);
+    }
+    else if (input[i] == ')')
+    {
+        int startIndex = indexes.Pop();
+        Console.WriteLine(input.Substring(startIndex, i - startIndex + 1));
+    }
+}
