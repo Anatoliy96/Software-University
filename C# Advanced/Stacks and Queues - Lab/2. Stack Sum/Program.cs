603c7f1fc3affe77155278ae3e23406a66515415
Anatoliy <leaved@abv.bg>
2023-05-11 15:23:51 +0300

    2. Stack Sum

---
+++ C# Advanced/Stacks and Queues - Lab/2. Stack Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿
+int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Stack<int> numbers = new Stack<int>(input);
+
+string command = Console.ReadLine().ToLower();
+
+while (command != "end")
+{
+    string[] tokens = command.Split();
+
+    if (tokens[0] == "add")
+    {
+        int n1 = int.Parse(tokens[1]);
+        int n2 = int.Parse(tokens[2]);
+
+        numbers.Push(n1);
+        numbers.Push(n2);
+    }
+    else if (tokens[0] == "remove")
+    {
+        int count = int.Parse(tokens[1]);
+
+        if (numbers.Count > count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                numbers.Pop();
+            }
+        }
+    }
+
+    command = Console.ReadLine().ToLower();
+}
+
+Console.WriteLine("Sum: " + numbers.Sum());
