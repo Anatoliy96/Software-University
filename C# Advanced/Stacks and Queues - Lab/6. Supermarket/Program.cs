de3f814e634f0d2826b0dac8e6eda49f1ad9a453
Anatoliy <leaved@abv.bg>
2023-05-11 15:53:38 +0300

    6. Supermarket

---
+++ C# Advanced/Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string command = Console.ReadLine();
+
+Queue<string> clients = new Queue<string>();
+
+while (command != "End")
+{
+    if (command == "Paid")
+    {
+        while (clients.Count > 0)
+        {
+            Console.WriteLine(clients.Dequeue());
+        }
+    }
+    else
+    {
+        clients.Enqueue(command);
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine($"{clients.Count} people remaining.");
