2630f7dfdbdc14dae4388b30270a172f4e6a6f96
Anatoliy <leaved@abv.bg>
2023-05-12 12:34:33 +0300

    7. Hot Potato

---
+++ C# Advanced/Stacks and Queues - Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,24 @@
+﻿
+string[] input = Console.ReadLine().Split(' ');
+
+Queue<string> kids = new Queue<string>(input);
+
+int count = int.Parse(Console.ReadLine());
+int counter = 1;
+
+while (kids.Count > 1)
+{
+    if (count == counter)
+    {
+        Console.WriteLine($"Removed {kids.Dequeue()}");
+        counter = 1;
+    }
+    else
+    {
+        string currentKid = kids.Dequeue();
+        kids.Enqueue(currentKid);
+        counter++;
+    }
+}
+Console.WriteLine($"Last is {kids.Dequeue()}");
+
