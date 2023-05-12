864032b4f983f5d7d7f596c11f696a123f5a8053
Anatoliy <leaved@abv.bg>
2023-05-12 14:41:52 +0300

    05. Fashion Boutique

---
+++ C# Advanced/Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int capacity = int.Parse(Console.ReadLine());
+
+Stack<int> clothes = new Stack<int>(input);
+
+int rackCounter = 1;
+
+int sum = 0;
+
+
+while (clothes.Count > 0)
+{
+    int current = clothes.Pop();
+
+    if (sum + current <= capacity)
+    {
+        sum += current;
+    }
+    else
+    {
+        sum = current;
+
+        rackCounter++;
+    }
+}
+Console.WriteLine(rackCounter);
+
+
+
