7647e28304a12c9ee068e234bb5b1918c6649c3e
Anatoliy <leaved@abv.bg>
2023-05-12 12:55:01 +0300

    8. Traffic Jam

---
+++ C# Advanced/Stacks and Queues - Lab/8. Traffic Jam/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+string command = Console.ReadLine();
+
+Queue<string> cars = new Queue<string>();
+int passedCars = 0;
+
+while (command != "end")
+{
+    if (command == "green")
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (cars.Count == 0)
+            {
+                break;
+            }
+
+            Console.WriteLine($"{cars.Dequeue()} passed!");
+            passedCars++;
+        }
+    }
+    else
+    {
+        cars.Enqueue(command);
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine($"{passedCars} cars passed the crossroads.");
