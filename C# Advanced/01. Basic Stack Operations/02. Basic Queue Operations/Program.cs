dfdf10f5ae8e443d12a2c6e6c57cbef9b7f9c84a
Anatoliy <leaved@abv.bg>
2023-05-12 13:53:52 +0300

    02. Basic Queue Operations

---
+++ C# Advanced/01. Basic Stack Operations/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,47 @@
+﻿int[] command = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+Queue<int> numbers = new Queue<int>();
+
+int enqueue = command[0];
+int dequeue = command[1];
+int numberToFind = command[2];
+
+int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+for (int i = 0; i < enqueue; i++)
+{
+    numbers.Enqueue(input[i]);
+}
+for (int i = 0; i < dequeue; i++)
+{
+    numbers.Dequeue();
+}
+
+if (numbers.Count == 0)
+{
+    Console.WriteLine(0);
+    return;
+}
+else
+{
+    foreach (var num in numbers)
+    {
+        if (num == numberToFind)
+        {
+            Console.WriteLine("true");
+            return;
+        }
+
+    }
+}
+int min = int.MaxValue;
+
+foreach (var num in numbers)
+{
+    if (num < min)
+    {
+        min = num;
+    }
+}
+Console.WriteLine(min);
+
