498f3d3e311305b4d8c5ff36172d229c40755e59
Anatoliy <leaved@abv.bg>
2023-05-15 13:46:37 +0300

    3. Primary Diagonal

---
+++ C# Advanced/Multidimensional Arrays - Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,27 @@
+﻿int size = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[size, size];
+
+int sum = 0;
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    int[] cols = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = cols[col];
+    }
+}
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        if (row == col)
+        {
+            sum += matrix[row, col];
+        }
+    }
+}
+Console.WriteLine(sum);
