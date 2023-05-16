7903ff062e2545a2dd88762ab3cae564d4ed9841
Anatoliy <leaved@abv.bg>
2023-05-16 15:56:39 +0300

    5. Snake Moves

---
+++ C# Advanced/Multidimensional Arrays - Exercise/5. Snake Moves/Program.cs
@@ -0,0 +1,51 @@
+﻿int[] sizes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+char[,] matrix = new char[sizes[0], sizes[1]];
+
+string input = Console.ReadLine();
+
+int currentWordIndex = 0;
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    if (row % 2 == 0)
+    {
+        if (currentWordIndex == input.Length)
+        {
+            currentWordIndex = 0;
+        }
+
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+
+            if (currentWordIndex == input.Length)
+            {
+                currentWordIndex = 0;
+            }
+
+            matrix[row, col] = input[currentWordIndex];
+            currentWordIndex++;
+        }
+    }
+    else
+    {
+        for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+        {
+            if (currentWordIndex == input.Length)
+            {
+                currentWordIndex = 0;
+            }
+
+            matrix[row, col] = input[currentWordIndex];
+            currentWordIndex++;
+        }
+    }
+}
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        Console.Write(matrix[row, col]);
+    }
+    Console.WriteLine();
+}
