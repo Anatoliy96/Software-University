416d4673781fe3cc3b2f1c898f4cde30eaed08e9
Anatoliy <leaved@abv.bg>
2023-05-16 14:15:50 +0300

    1. Diagonal Difference

---
+++ C# Advanced/Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,36 @@
+﻿int size = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[size, size];
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    int[] cols = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = cols[col];
+    }
+}
+
+int rightDiagonalSum = 0;
+int leftDiagonalSum = 0;
+int diffrence = 0;
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+    {
+        if (row == col)
+        {
+            rightDiagonalSum += matrix[row, col];
+        }
+        if (row == row && col == matrix.GetLength(1) - 1 - row)
+        {
+            leftDiagonalSum += matrix[row, col];
+        }
+    }
+}
+
+diffrence = Math.Abs(rightDiagonalSum - leftDiagonalSum);
+
+Console.WriteLine(diffrence);
