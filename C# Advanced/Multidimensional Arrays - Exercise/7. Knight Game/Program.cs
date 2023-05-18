d0e49d0f55fb9b7b378cf15d0afb452131099a8d
Anatoliy <leaved@abv.bg>
2023-05-18 19:21:17 +0300

    7. Knight Game

---
+++ C# Advanced/Multidimensional Arrays - Exercise/7. Knight Game/Program.cs
@@ -0,0 +1,127 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+if (n < 3)
+{
+    Console.WriteLine(0);
+    return;
+}
+
+char[,] matrix = new char[n, n];
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    char[] cols = Console.ReadLine().ToArray();
+
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = cols[col];
+
+    }
+}
+
+int knightsRemoved = 0;
+
+while (true)
+{
+    int countMostAttacking = 0;
+    int rowMostAttacking = 0;
+    int colMostAttaking = 0;
+
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            if (matrix[row, col] == 'K')
+            {
+                int attackedKnights = CountOfAttackedKnights(row, col, n, matrix);
+
+                if (countMostAttacking < attackedKnights)
+                {
+                    countMostAttacking = attackedKnights;
+                    rowMostAttacking = row;
+                    colMostAttaking = col;
+                }
+            }
+        }
+    }
+
+    if (countMostAttacking == 0)
+    {
+        break;
+    }
+    else
+    {
+        matrix[rowMostAttacking, colMostAttaking] = '0';
+        knightsRemoved++;
+    }
+}
+Console.WriteLine(knightsRemoved);
+
+
+static int CountOfAttackedKnights(int row, int col, int n, char[,] matrix)
+{
+    int attackedKnights = 0;
+
+    if (IsCellValid(row - 1, col - 2, n))
+    {
+        if (matrix[row - 1, col - 2] == 'K')
+        {
+            attackedKnights++;
+        }
+    }
+    if (IsCellValid(row + 1, col - 2, n))
+    {
+        if (matrix[row + 1, col - 2] == 'K')
+        {
+            attackedKnights++;
+        }
+    }
+    if (IsCellValid(row - 1, col + 2, n))
+    {
+        if (matrix[row - 1, col + 2] == 'K')
+        {
+            attackedKnights++;
+        }
+    }
+    if (IsCellValid(row + 1, col + 2, n))
+    {
+        if (matrix[row + 1, col + 2] == 'K')
+        {
+            attackedKnights++;
+        }
+    }
+    if (IsCellValid(row - 2, col - 1, n))
+    {
+        if (matrix[row - 2, col - 1] == 'K')
+        {
+            attackedKnights++;
+        }
+    }
+    if (IsCellValid(row - 2, col + 1, n))
+    {
+        if (matrix[row - 2, col + 1] == 'K')
+        {
+            attackedKnights++;
+        }
+    }
+    if (IsCellValid(row + 2, col - 1, n))
+    {
+        if (matrix[row + 2, col - 1] == 'K')
+        {
+            attackedKnights++;
+        }
+    }
+    if (IsCellValid(row + 2, col + 1, n))
+    {
+        if (matrix[row + 2, col + 1] == 'K')
+        {
+            attackedKnights++;
+        }
+    }
+    return attackedKnights;
+}
+
+static bool IsCellValid(int row, int col, int n)
+{
+    return (row >= 0 && row < n && col >= 0 && col < n);
+}
