fb6d3fc3ff18962194cd6fd2ecb07aa401a37016
Anatoliy <leaved@abv.bg>
2023-05-20 14:49:01 +0300

    04. Product Shop

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
@@ -0,0 +1,41 @@
+﻿string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string,double>>();
+
+while (input[0] != "Revision")
+{
+    string shopName = input[0];
+    string product = input[1];
+    double price = double.Parse(input[2]);
+
+    if (!shops.ContainsKey(shopName))
+    {
+        shops.Add(shopName, new Dictionary<string, double>());
+
+        if (!shops[shopName].ContainsKey(product))
+        {
+            shops[shopName].Add(product, price);
+        }
+    }
+    else
+    {
+        if (!shops[shopName].ContainsKey(product))
+        {
+            shops[shopName].Add(product, price);
+        }
+    }
+
+    input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+}
+
+shops = shops.OrderBy(s => s.Key).ToDictionary(s => s.Key, s => s.Value);
+
+foreach (var shop in shops)
+{
+    Console.WriteLine($"{shop.Key}->");
+
+    foreach (var product in shop.Value)
+    {
+        Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+    }
+}
