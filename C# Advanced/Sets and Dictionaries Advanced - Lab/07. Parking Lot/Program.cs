e9227499f6c12687d1509b2200a7aa429c7f6a75
Anatoliy <leaved@abv.bg>
2023-05-20 15:29:55 +0300

    07. Parking Lot

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,33 @@
+﻿List<string> input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+HashSet<string> carPlates = new HashSet<string>();
+
+while (input[0] != "END")
+{
+    string command = input[0];
+    string car = input[1];
+
+    if (command == "IN")
+    {
+        carPlates.Add(car);
+    }
+    else if (command == "OUT")
+    {
+        if (carPlates.Contains(car))
+        {
+            carPlates.Remove(car);
+        }
+    }
+    input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+}
+if (carPlates.Count() > 0)
+{
+    foreach (var car in carPlates)
+    {
+        Console.WriteLine($"{car}");
+    }
+}
+else
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
