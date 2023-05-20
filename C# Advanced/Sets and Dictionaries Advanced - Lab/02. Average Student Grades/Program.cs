be2a708ec88f7f1ce11fc0e2a25007e968ca5f95
Anatoliy <leaved@abv.bg>
2023-05-20 14:22:22 +0300

    02. Average Student Grades

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<decimal>> grades = new Dictionary<string, List<decimal>>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string studentName = input[0];
+    decimal grade = decimal.Parse(input[1]);
+
+    if (!grades.ContainsKey(studentName))
+    {
+        grades.Add(studentName, new List<decimal>());
+    }
+
+    grades[studentName].Add(grade);
+}
+
+foreach (var student in grades)
+{
+    Console.WriteLine($"{student.Key} -> {string.Join(" ", student.Value.Select(g => g.ToString("F2")))} (avg: {student.Value.Average():f2})");
+}
