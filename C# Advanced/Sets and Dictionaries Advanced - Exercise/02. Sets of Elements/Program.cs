0dc79eaa06c54154e38bb3819b1119868b385689
Anatoliy <leaved@abv.bg>
2023-05-20 21:10:52 +0300

    02. Sets of Elements

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+int firstLength = input[0];
+int secondLength = input[1];
+
+HashSet<int> n = new HashSet<int>();
+HashSet<int> m = new HashSet<int>();
+
+HashSet<int> occurences = new HashSet<int>();
+
+for (int i = 0; i < firstLength; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    n.Add(number);
+}
+for (int i = 0; i < secondLength; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    m.Add(number);
+}
+
+foreach (var item in n)
+{
+    foreach (var item2 in m)
+    {
+        if (item == item2)
+        {
+            occurences.Add(item);
+        }
+    }
+}
+
+Console.WriteLine(String.Join(" ", occurences));
