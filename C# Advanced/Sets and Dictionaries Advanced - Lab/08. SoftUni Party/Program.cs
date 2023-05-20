8ef3b24d8b65130307b32c8f2400a228d025bac1
Anatoliy <leaved@abv.bg>
2023-05-20 15:47:14 +0300

    08. SoftUni Party

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,40 @@
+﻿string command = Console.ReadLine();
+
+HashSet<string> guests = new HashSet<string>();
+
+while (command != "PARTY")
+{
+    guests.Add(command);
+
+    command = Console.ReadLine();
+}
+
+command = Console.ReadLine();
+
+while (command != "END")
+{
+    if (guests.Contains(command))
+    {
+        guests.Remove(command);
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine(guests.Count);
+foreach (var item in guests)
+{
+    char[] ch = item.ToCharArray();
+    if (char.IsDigit(ch[0]))
+    {
+        Console.WriteLine(item);
+    }
+}
+foreach (var item in guests)
+{
+    char[] ch = item.ToCharArray();
+    if (char.IsLetter(ch[0]))
+    {
+        Console.WriteLine(item);
+    }
+}
