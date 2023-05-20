75ff5d35a94af1d5631a5ea1b8ce88aff4338724
Anatoliy <leaved@abv.bg>
2023-05-20 14:25:40 +0300

    03. Largest 3 Numbers

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+int[] sorted = input.OrderByDescending(x => x).ToArray();
+
+Console.WriteLine($"{string.Join(" ", sorted.Take(3))}");
