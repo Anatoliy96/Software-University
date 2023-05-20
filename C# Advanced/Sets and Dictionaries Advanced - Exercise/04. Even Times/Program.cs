555c873d9e6984c8599b94b50a7492c723ee52e3
Anatoliy <leaved@abv.bg>
2023-05-20 22:24:31 +0300

    04. Even Times

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,25 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Dictionary<int, int> evenTimes = new Dictionary<int, int>();
+
+for (int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (!evenTimes.ContainsKey(number))
+    {
+        evenTimes.Add(number, 1);
+    }
+    else
+    {
+        evenTimes[number]++;
+    }
+}
+foreach (var item in evenTimes)
+{
+    if (item.Value % 2 == 0)
+    {
+        Console.WriteLine(item.Key);
+        return;
+    }
+}
