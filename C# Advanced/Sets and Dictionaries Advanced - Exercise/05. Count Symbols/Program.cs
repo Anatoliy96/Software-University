575d59f04aaee8582860cdec0bb0177f2e07f6a6
Anatoliy <leaved@abv.bg>
2023-05-20 22:30:48 +0300

    05. Count Symbols

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,20 @@
+﻿string input = Console.ReadLine();
+
+SortedDictionary<char, int> countSymbols = new SortedDictionary<char, int>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (!countSymbols.ContainsKey(input[i]))
+    {
+        countSymbols.Add(input[i], 1);
+    }
+    else
+    {
+        countSymbols[input[i]]++;
+    }
+}
+
+foreach (var symbol in countSymbols)
+{
+    Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+}
