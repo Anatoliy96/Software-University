571b56986653499dac18898f98a7c02f6a5633fb
Anatoliy <leaved@abv.bg>
2023-05-21 20:13:21 +0300

    09. SoftUni Exam Results

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/09. SoftUni Exam Results/Program.cs
@@ -0,0 +1,60 @@
+﻿string[] input = Console.ReadLine().Split("-", StringSplitOptions.RemoveEmptyEntries);
+
+Dictionary<string, int> users = new Dictionary<string, int>();
+Dictionary<string, int> contestCount = new Dictionary<string, int>();
+
+while (input[0] != "exam finished")
+{
+    if (input[1].Contains("banned"))
+    {
+        string username = input[0];
+
+        if (users.ContainsKey(username))
+        {
+            users.Remove(username);
+        }
+    }
+    else
+    {
+        string username = input[0];
+        string contest = input[1];
+        int points = int.Parse(input[2]);
+
+        if (!users.ContainsKey(username))
+        {
+            users.Add(username, points);
+        }
+        if (users.ContainsKey(username))
+        {
+            if (users[username] < points)
+            {
+                users[username] = points;
+            }
+        }
+        if (!contestCount.ContainsKey(contest))
+        {
+            contestCount.Add(contest, 1);
+        }
+        else
+        {
+            contestCount[contest]++;
+        }
+    }
+    
+
+    input = Console.ReadLine().Split("-", StringSplitOptions.RemoveEmptyEntries);
+}
+
+Console.WriteLine("Results:");
+
+foreach (var user in users.OrderByDescending(u => u.Value).ThenBy(u => u.Key))
+{
+    Console.WriteLine($"{user.Key} | {user.Value}");
+}
+
+Console.WriteLine("Submissions:");
+
+foreach (var contest in contestCount.OrderByDescending(u => u.Value).ThenBy(u => u.Key))
+{
+    Console.WriteLine($"{contest.Key} - {contest.Value}");
+}
