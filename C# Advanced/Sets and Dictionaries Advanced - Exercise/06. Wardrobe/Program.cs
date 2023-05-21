9e97e67a00f8d8b1b6b2c1e689003c1282cc2912
Anatoliy <leaved@abv.bg>
2023-05-21 13:09:09 +0300

    06. Wardrobe

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/06. Wardrobe/Program.cs
@@ -0,0 +1,66 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+    string[] clothes = input[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+    string color = input[0];
+
+    if (!wardrobe.ContainsKey(color))
+    {
+        wardrobe.Add(color, new Dictionary<string, int>());
+
+        for (int j = 0; j < clothes.Length; j++)
+        {
+            if (!wardrobe[color].ContainsKey(clothes[j]))
+            {
+                wardrobe[color].Add(clothes[j], 1);
+            }
+            else
+            {
+                wardrobe[color][clothes[j]]++;
+            }
+        }
+    }
+    else
+    {
+        for (int j = 0; j < clothes.Length; j++)
+        {
+            if (!wardrobe[color].ContainsKey(clothes[j]))
+            {
+                wardrobe[color].Add(clothes[j], 1);
+            }
+            else
+            {
+                wardrobe[color][clothes[j]]++;
+            }
+        }
+    }
+
+}
+string[] clothToFind = Console.ReadLine().Split(" ");
+
+string colorToFind = clothToFind[0];
+string clothInWardrobe = clothToFind[1];
+
+foreach (var color in wardrobe)
+{
+    Console.WriteLine($"{color.Key} clothes:");
+
+    foreach (var cloth in color.Value)
+    {
+        if (color.Key.Contains(colorToFind) && cloth.Key.Contains(clothInWardrobe))
+        {
+            Console.WriteLine($"* {cloth.Key} - {cloth.Value} (found!)");
+        }
+        else
+        {
+            Console.WriteLine($"* {cloth.Key} - {cloth.Value}");
+        }
+
+    }
+}
