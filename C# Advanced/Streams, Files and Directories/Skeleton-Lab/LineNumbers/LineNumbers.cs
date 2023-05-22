321d9fffcc80845ae04785f9e887b90def69d503
Anatoliy <leaved@abv.bg>
2023-05-22 20:35:49 +0300

    01. Odd Lines

---
+++ C# Advanced/Streams, Files and Directories/Skeleton-Lab/LineNumbers/LineNumbers.cs
@@ -0,0 +1,30 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            int count = 1;
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                string line = reader.ReadLine();
+
+                while (!reader.EndOfStream)
+                {
+                    System.Console.WriteLine($"{count} {reader.ReadLine()}");
+                    count++;
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/Streams, Files and Directories/Skeleton-Lab/OddLines/OddLines.cs
@@ -0,0 +1,40 @@
+﻿namespace OddLines
+{
+    using System.IO;
+	
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int countOfRow = 0;
+                    
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (countOfRow++ % 2 != 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+}
