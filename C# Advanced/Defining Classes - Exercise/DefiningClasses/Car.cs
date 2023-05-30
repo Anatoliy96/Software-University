f5b9eb50702c66b9268a6a972289f4c870554ab0
Anatoliy <leaved@abv.bg>
2023-05-30 19:38:25 +0300

    06. Speed Racing

---
+++ C# Advanced/Defining Classes - Exercise/DefiningClasses/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public Car()
+        {
+            travelledDistance = 0;
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer) : this()
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+        }
+
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public double FuelAmount
+        {
+            get { return fuelAmount; }
+            set { fuelAmount = value; }
+        }
+
+        public double FuelConsumptionPerKilometer
+        {
+            get { return fuelConsumptionPerKilometer; }
+            set { fuelConsumptionPerKilometer = value; }
+        }
+        
+        public double TravelledDistance
+        {
+            get { return travelledDistance; }
+            set { travelledDistance = value; }
+        }
+
+        public bool CanMove(double distance)
+        {
+            double fuelNeeded = fuelConsumptionPerKilometer * distance;
+            return fuelAmount >= fuelNeeded;
+        }
+
+        public void Move(double distance)
+        {
+            double fuelNeeded = distance * FuelConsumptionPerKilometer;
+            FuelAmount -= fuelNeeded;
+            TravelledDistance += distance;
+        }
+    }
+}
--- C# Advanced/Defining Classes - Exercise/DefiningClasses/Program.cs
+++ C# Advanced/Defining Classes - Exercise/DefiningClasses/Program.cs
@@ -4,14 +4,49 @@
     {
         static void Main()
         {
-            string firstDate = Console.ReadLine();
-            string secondDate = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
 
-            DateModifier dateModifier = new DateModifier();
+            List<Car> cars = new List<Car>();
 
-            int diffrence = dateModifier.DiffrenceBetweenTwoDates(firstDate, secondDate);
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-            Console.WriteLine(diffrence);
+                string model = input[0];
+                double fuelAmount = double.Parse(input[1]);
+                double fuelConsumptionPerKilometer = double.Parse(input[2]);
+
+                Car car = new Car(model, fuelAmount, fuelConsumptionPerKilometer);
+
+                cars.Add(car);
+
+            }
+
+            string[] commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (commands[0] != "End")
+            {
+                string model = commands[1];
+                double amountOfKilometers = double.Parse(commands[2]);
+
+                Car car = cars.Find(x => x.Model == model);
+
+                if (car.CanMove(amountOfKilometers))
+                {
+                    car.Move(amountOfKilometers);
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient fuel for the drive");
+                }
+
+                commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
         }
     }
 }
