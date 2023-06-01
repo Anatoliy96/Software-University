8e1b5ab490258681e3cbb045f8b28b94a0b92940
Anatoliy <leaved@abv.bg>
2023-06-01 13:54:04 +0300

    09. Pokemon Trainer

---
+++ C# Advanced/Defining Classes - Exercise/09. PokemonTrainer/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer;
+
+    public class Startup
+{
+    static void Main(string[] args)
+    {
+        List<Trainer> trainers = new List<Trainer>();
+
+        string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        while (input[0] != "Tournament")
+        {
+            Trainer trainer = trainers.SingleOrDefault(t => t.Name == input[0]);
+
+            if (trainer == null)
+            {
+                trainer = new Trainer(input[0]);
+                trainer.Pokemons.Add(new(input[1], input[2], int.Parse(input[3])));
+                trainers.Add(trainer);
+            }
+            else
+            {
+                trainer.Pokemons.Add(new(input[1], input[2], int.Parse(input[3])));
+            }
+
+            input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        string secondInput = Console.ReadLine();
+
+        while (secondInput != "End")
+        {
+            foreach (var trainer in trainers)
+            {
+                trainer.CheckPokemon(secondInput);
+            }
+            secondInput = Console.ReadLine();
+        }
+
+        foreach (var trainer in trainers.OrderByDescending(b => b.NumberOfBadges))
+        {
+            Console.WriteLine($"{trainer.Name} {trainer.NumberOfBadges} {trainer.Pokemons.Count}");
+        }
+    }
+}
+
