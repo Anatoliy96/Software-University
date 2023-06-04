8c37fba2d07664c7f997e7b5ee27935e46cfc9fc
Anatoliy <leaved@abv.bg>
2023-06-04 20:34:36 +0300

    02. Sum Numbers

---
+++ C# Advanced/Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(n => int.Parse(n)).ToArray();
+
+Console.WriteLine(numbers.Count());
+Console.WriteLine(numbers.Sum());
+
+    
+    
+    
