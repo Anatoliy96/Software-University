fc0066b8797e8ce99d7515de463d4058e5ee00ae
Anatoliy <leaved@abv.bg>
2023-06-04 20:29:34 +0300

    01. Sort Even Numbers

--- C# Advanced/Functional Programming - Lab/01. Sort Even Numbers/Program.cs
+++ C# Advanced/Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .Where(n => n % 2 == 0)
+    .OrderBy(n => n).ToArray();
+
+Console.WriteLine(String.Join(", ", numbers));
