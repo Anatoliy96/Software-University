015d948b5de9d5c5eb82676514bbf825c0bd71bc
Anatoliy <leaved@abv.bg>
2023-06-06 18:04:33 +0300

    03. Generic Scale

---
+++ C# Advanced/Generics - Lab/GenericScale/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericScale
+{
+    public class StartUp 
+    {
+        public static void Main()
+        {
+            EqualityScale<int> equal = new EqualityScale<int>(6, 5);
+            Console.WriteLine(equal.AreEqual());
+        }
+    }
+}
