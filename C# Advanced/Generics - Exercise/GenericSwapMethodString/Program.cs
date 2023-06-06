6140ca4322b28d7790b9989a43deb46bd5509a5f
Anatoliy <leaved@abv.bg>
2023-06-06 20:33:36 +0300

    03. Generic Swap Method String

---
+++ C# Advanced/Generics - Exercise/GenericSwapMethodString/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace GenericSwapMethodString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> elemenets = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string text = Console.ReadLine();
+
+                elemenets.Add(text);
+            }
+
+            int[] indexes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+             int index1 = indexes[0];
+            int index2 = indexes[1];
+
+            SwapElements(elemenets, index1, index2);
+
+            foreach (var element in elemenets)
+            {
+                Console.WriteLine($"{element.GetType().FullName}: {element}");
+            }
+
+        }
+
+        public static T SwapElements<T>(List<T> elements, int index1, int index2)
+        {
+            T temp = elements[index1];
+            elements[index1] = elements[index2];
+            elements[index2] = temp;
+
+            return temp;
+        }
+    }
+}
