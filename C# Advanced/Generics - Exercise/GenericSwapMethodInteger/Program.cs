2fa69c6d1d6a87d68b730f528d19dbfbeb729442
Anatoliy <leaved@abv.bg>
2023-06-06 20:36:30 +0300

    04. Generic Swap Method Integer

---
+++ C# Advanced/Generics - Exercise/GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace GenericSwapMethodInteger
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> elemenets = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                elemenets.Add(number);
+            }
+
+            int[] indexes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int index1 = indexes[0];
+            int index2 = indexes[1];
+
+            SwapElements(elemenets, index1, index2);
+
+            foreach (var element in elemenets)
+            {
+                Console.WriteLine($"{element.GetType().FullName}: {element}");
+            }
+        }
+
+        public static T SwapElements<T>(List<T> elements, int index1, int index2)
+        {
+            T temp = elements[index1];
+            elements[index1] = elements[index2];
+            elements[index2] = temp;
+
+            return temp;
+        }
+    }
+}
