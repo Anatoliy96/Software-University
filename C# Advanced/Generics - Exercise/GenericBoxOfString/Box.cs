fd2fec9b8e5d6394d71ba4cb839505022bc56149
Anatoliy <leaved@abv.bg>
2023-06-06 18:47:04 +0300

    01. Generic Box of String

---
+++ C# Advanced/Generics - Exercise/GenericBoxOfString/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private T name;
+
+        public Box(T name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{typeof(T).FullName}: {name}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
