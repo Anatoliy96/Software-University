ac02e30621e86cfc81609ad4b30b504bc5e8dc10
Anatoliy <leaved@abv.bg>
2023-06-06 19:00:11 +0300

    02. Generic Box of Integer

---
+++ C# Advanced/Generics - Exercise/GenericBoxOfInteger/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                Box<int> box = new Box<int>(number);
+
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
