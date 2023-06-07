25bb47eeec0609fd16803f523dd3ff5df3f6984f
Anatoliy <leaved@abv.bg>
2023-06-07 12:46:53 +0300

    06. Generic Count Method Double

---
+++ C# Advanced/Generics - Exercise/GenericCountMethodDouble/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericCountMethodDouble
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<double> box = new Box<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double element = double.Parse(Console.ReadLine());
+                box.Add(element);
+            }
+
+            double comparedElemenet = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(box.CountOfComparedElemenets(comparedElemenet));
+        }
+    }
+}
