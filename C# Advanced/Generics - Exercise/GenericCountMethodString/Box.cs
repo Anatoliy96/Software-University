2d143f5d9c4b04478a6a26b65e2bb1358ce854ac
Anatoliy <leaved@abv.bg>
2023-06-07 12:43:50 +0300

    05. Generic Count Method String

---
+++ C# Advanced/Generics - Exercise/GenericCountMethodString/Box.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCountMethodString
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        private List<T> elemenets;
+
+        public Box()
+        {
+            this.elemenets = new List<T>();
+        }
+        public void Add(T item)
+        {
+            elemenets.Add(item);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (T item in elemenets)
+            {
+                sb.AppendLine($"{typeof(T).FullName}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+        public int CountOfComparedElemenets(T element)
+        {
+            int count = 0;
+
+            foreach (T item in elemenets)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ C# Advanced/Generics - Exercise/GenericCountMethodString/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericCountMethodString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string element = Console.ReadLine();
+                box.Add(element);
+            }
+
+            string comparedElemenet = Console.ReadLine();
+
+            Console.WriteLine(box.CountOfComparedElemenets(comparedElemenet));
+        }
+    }
+}
