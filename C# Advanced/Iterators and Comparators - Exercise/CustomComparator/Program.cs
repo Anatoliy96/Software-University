26d4f036194e70ebd8a42ec619618e516d39f671
Anatoliy <leaved@abv.bg>
2023-06-08 22:37:43 +0300

    07. Custom Comparator

---
+++ C# Advanced/Iterators and Comparators - Exercise/CustomComparator/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CustomComparator
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Comparator comparator = new Comparator();
+
+            Array.Sort(numbers, comparator);
+
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+
+        public class Comparator : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                if (x % 2 == 0 && y % 2 != 0)
+                {
+                    return -1;
+                }
+                else if (x % 2 != 0 && y % 2 == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return x.CompareTo(y);
+                }
+            }
+        }
+    }
+}
