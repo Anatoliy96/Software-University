2f3b683909d8d7df2a7d49cdc94467aff31a1abe
Anatoliy <leaved@abv.bg>
2023-06-08 14:01:25 +0300

    03. ComparableBook

--- C# Advanced/Iterators and Comparators - Lab/IteratorsAndComparators/Book.cs
+++ C# Advanced/Iterators and Comparators - Lab/IteratorsAndComparators/Book.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace IteratorsAndComparators
 {
-    public class Book
+    public class Book : IComparable<Book>
     {
         public Book(string title, int year, params string[] authors)
         {
@@ -18,5 +18,21 @@ namespace IteratorsAndComparators
         public string Title { get; set; }
         public int Year { get; set; }
         public List<string> Authors { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            int result = Year.CompareTo(other.Year);
+
+            if (result == 0)
+            {
+                result = Title.CompareTo(other.Title);
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
     }
 }
