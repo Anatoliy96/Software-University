93f11021a3925d2f4724680196e95417922d720f
Anatoliy <leaved@abv.bg>
2023-06-08 13:37:54 +0300

    02. LibraryIterator

--- C# Advanced/Iterators and Comparators - Lab/IteratorsAndComparators/Library.cs
+++ C# Advanced/Iterators and Comparators - Lab/IteratorsAndComparators/Library.cs
@@ -18,15 +18,44 @@ namespace IteratorsAndComparators
 
         public IEnumerator<Book> GetEnumerator()
         {
-            foreach (var book in books)
-            {
-                yield return book;
-            }
+            return new LibraryIterator(this.books);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+    }
+
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private List<Book> books;
+        private int index;
+
+        public LibraryIterator(List<Book> books)
+        {
+            this.books = books;
+            this.Reset();
+        }
+
+        public Book Current => this.books[this.index];
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+           
+        }
+
+        public bool MoveNext()
+        {
+            index++;
+            return this.index < this.books.Count;
+        }
+
+        public void Reset()
+        {
+            this.index = -1;
         }
     }
 }
--- C# Advanced/Iterators and Comparators - Lab/IteratorsAndComparators/Program.cs
+++ C# Advanced/Iterators and Comparators - Lab/IteratorsAndComparators/Program.cs
@@ -10,6 +10,12 @@
             Book bookThree = new Book("The Documents in the Case", 1930);
             Library libraryOne = new Library();
             Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+            foreach (var book in libraryTwo)
+            {
+                Console.WriteLine(book.Title);
+            }
+
         }
     }
 }
