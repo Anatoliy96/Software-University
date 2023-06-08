daac526fa8f92d9224bd3bda13d7cf1e4348cd7b
Anatoliy <leaved@abv.bg>
2023-06-08 21:58:09 +0300

    04. Froggy

---
+++ C# Advanced/Iterators and Comparators - Exercise/Froggy/Lake.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> rocks;
+
+        public Lake()
+        {
+            this.rocks = new List<int>();
+        }
+
+        public void Add(int rock)
+        {
+            rocks.Add(rock);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < rocks.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return rocks[i];
+                }
+            }
+
+            for (int i = rocks.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return rocks[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercise/Froggy/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Froggy
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Lake lake = new Lake();
+
+            foreach (var item in input)
+            {
+                lake.Add(item);
+            }
+
+            foreach (var rock in lake)
+            {
+                string result = string.Join(", ", lake);
+                Console.WriteLine(result);
+                return;
+            }
+        }
+    }
+}
