73203ba326b24782584a8adf1451311d67d3aaaf
Anatoliy <leaved@abv.bg>
2023-06-08 17:51:48 +0300

    01. ListyIterator

---
+++ C# Advanced/Iterators and Comparators - Exercise/ListyIterator/ListyIterator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> list;
+        private int index;
+
+        public ListyIterator()
+        {
+            this.list = new List<T>();
+            this.index = 0;
+        }
+
+        public void Create(T element)
+        {
+            list.Add(element);
+        }
+        public bool Move()
+        {
+            if (index < list.Count - 1)
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasNext()
+        {
+            return index < list.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(list[index]);
+            }
+            
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercise/ListyIterator/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ListyIterator
+{
+    public class StarUp
+    {
+        public static void Main()
+        {
+            string[] commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> elemenets = new List<string>();
+
+            for (int i = 1; i <= commands.Length - 1; i++)
+            {
+                elemenets.Add(commands[i]);
+            }
+
+            ListyIterator<string> listyInterator = new ListyIterator<string>();
+            foreach (var item in elemenets)
+            {
+                listyInterator.Create(item);
+            }
+
+            commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (commands[0] != "END")
+            {
+                if (commands[0] == "Print")
+                {
+                    listyInterator.Print();
+                }
+                else if (commands[0] == "HasNext")
+                {
+                    bool result = listyInterator.HasNext();
+
+                    Console.WriteLine(result);
+                }
+                else if (commands[0] == "Move")
+                {
+                    bool result = listyInterator.Move();
+
+                    Console.WriteLine(result);
+                }
+
+                commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
