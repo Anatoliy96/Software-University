0af10e3af04b1f212854f64560774d03ec4f765d
Anatoliy <leaved@abv.bg>
2023-06-11 21:24:08 +0300

    03. Clothes Magazine

---
+++ C# Advanced/Exam Preparation/Clothes Magazine/Magazine.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClothesMagazine
+{
+    public class Magazine
+    {
+        public Magazine(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+            Clothes = new List<Cloth>();
+        }
+
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public List<Cloth> Clothes { get; set; }
+
+        public void AddCloth(Cloth cloth)
+        {
+            Clothes.Add(cloth);
+        }
+        public bool RemoveCloth(string color)
+        {
+            Cloth clothToRemove = Clothes.FirstOrDefault(c => c.Color == color);
+            if (clothToRemove != null)
+            {
+                Clothes.Remove(clothToRemove);
+                return true;
+            }
+            return false;
+        }
+
+        public Cloth GetSmallestCloth()
+        {
+            return Clothes.OrderBy(c => c.Size).FirstOrDefault();
+        }
+
+        public Cloth GetCloth(string color)
+        {
+            return Clothes.FirstOrDefault(c => c.Color == color);
+        }
+
+        public int GetClothCount()
+        {
+            return Clothes.Count;
+        }
+
+        public string Report()
+        {
+            string result = $"{Type} magazine contains:";
+            foreach (var cloth in Clothes.OrderBy(c => c.Size))
+            {
+                result += Environment.NewLine + cloth.ToString();
+            }
+            return result;
+        }
+    }
+}
