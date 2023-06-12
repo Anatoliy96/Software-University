6029ab11d3cdb0391fa810ee2274604708bdac05
Anatoliy <leaved@abv.bg>
2023-06-12 20:20:01 +0300

    03. SoftUni Kindergarten not working 46/100

---
+++ C# Advanced/Exam Preparation/SoftUniKindergarten/Kindergarten.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUniKindergarten
+{
+    public class Kindergarten
+    {
+        public Kindergarten(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Registry = new List<Child>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public List<Child> Registry { get; set; }
+
+        public bool AddChild(Child child)
+        {
+            if (Registry.Count < Capacity)
+            {
+                Registry.Add(child);
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemoveChild(string childFullName)
+        {
+            Child child = Registry.FirstOrDefault(c => c.FirstName + " " + c.LastName == childFullName);
+
+            if (child != null)
+            {
+                Registry.Remove(child);
+                return true;
+            }
+
+            return false;
+        }
+
+        public int ChildrenCount()
+        {
+            return Registry.Count;
+        }
+
+        public Child GetChild(string childFullName)
+        {
+            Child child = Registry.FirstOrDefault(c => c.FirstName + " " + c.LastName == childFullName);
+
+            if (child != null)
+            {
+                return child;
+            }
+            
+            return null;
+        }
+
+        public string RegistryReport()
+        {
+            var orderedChildren = Registry.OrderByDescending(c => c.Age).ThenBy(c => c.LastName).ThenBy(c => c.FirstName).ToList();
+
+            string report = $"Registered children in {Name}:\n";
+            report += string.Join("\n", orderedChildren);
+            return report;
+        }
+    }
+}
