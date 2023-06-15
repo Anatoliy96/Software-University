78e1a51eade0e8a713fad21f49798d7b2c3bcc54
Anatoliy <leaved@abv.bg>
2023-06-15 22:10:00 +0300

    01. Action Print

---
+++ C# Advanced/Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] names = Console.ReadLine().Split(' ');
+
+Action<string> action = PrintNames(names);
+
+static Action<string> PrintNames(string[] names)
+{
+    foreach (var name in names)
+    {
+        Console.WriteLine(name);
+    }
+    return null;
+}
