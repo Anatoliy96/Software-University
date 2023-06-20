3e2812e714dc3dc4294e53e17171555a35a83caa
Anatoliy <leaved@abv.bg>
2023-06-20 20:54:05 +0300

    05. Stack of Strings

---
+++ C# OOP/Inheritance - Lab/CustomStack/StackOfStrings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> collection)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            foreach (var item in collection)
+            {
+                stackOfStrings.Push(item);
+            }
+
+            return stackOfStrings;
+        }
+    }
+}
