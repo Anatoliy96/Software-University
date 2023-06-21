f3e0855505ab986b04b4358fab1008e8fe398c82
Anatoliy <leaved@abv.bg>
2023-06-21 16:23:37 +0300

    NeedForSpeed fixed 100/100

---
+++ C# OOP/Inheritance - Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public abstract class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            HoursePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public double Fuel { get; set; }
+        public int HoursePower { get; set; }
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public virtual void Drive(double kilometers)
+        => Fuel -= kilometers * FuelConsumption;
+    }
+}
---
+++ C# OOP/Inheritance - Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        public Coffee(string name, decimal price, double milliliters, double caffeine) : base(name, price, milliliters)
+        {
+            price = 3.50m;
+            milliliters = 50;
+            Caffeine = caffeine;
+        }
+        
+        public double Caffeine { get; set; }
+    }
+}
