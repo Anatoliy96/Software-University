3a0183bd529fc9757c9aa88a2287f0e02fa1311b
Anatoliy <leaved@abv.bg>
2023-06-28 15:50:52 +0300

    01. Define an Interface IPerson

---
+++ C# OOP/Interfaces and Abstraction - Exercise/PersonInfo/Citizen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            private set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            private set { this.age = value; }
+        }
+    }
+}
