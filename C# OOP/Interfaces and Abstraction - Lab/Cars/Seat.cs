97f592960385bef46324d6e5cc9e474c43168514
Anatoliy <leaved@abv.bg>
2023-06-28 15:40:54 +0300

    02. Cars

---
+++ C# OOP/Interfaces and Abstraction - Lab/Cars/Seat.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        private string model;
+        private string color;
+
+        public Seat(string model, string color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+
+        public string Model 
+        { 
+            get
+            {
+                return this.model;
+            }
+            private set
+            {
+                this.model = value;
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                return this.color;
+            }
+            set
+            {
+                this.color = value;
+            }
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} {this.GetType().Name} {Model}";
+        }
+
+    }
+}
