b59ae9d7cd3b395f7a80a3d42d8fc2f4decd006a
Anatoliy <leaved@abv.bg>
2023-06-29 16:31:31 +0300

    06. Food Shortage

---
+++ C# OOP/Interfaces and Abstraction - Exercise/FoodShortage/IPersonality.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodShortage
+{
+    public interface IPersonality
+    {
+        string Name { get; }
+        int Age { get; }
+    }
+}
---
+++ C# OOP/Interfaces and Abstraction - Exercise/FoodShortage/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace FoodShortage
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<IBuyer> buyers = new List<IBuyer>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (command.Length == 4)
+                {
+                    string name = command[0];
+                    int age = int.Parse(command[1]);
+                    string id = command[2];
+                    string birthdate = command[3];
+
+                    Citizen citizen = new Citizen(name, age, id, birthdate);
+
+                    buyers.Add(citizen);
+                }
+                else if (command.Length == 3)
+                {
+                    string name = command[0];
+                    int age = int.Parse(command[1]);
+                    string group = command[2];
+
+                    Rebel rebel = new Rebel(name, age, group);
+
+                    buyers.Add(rebel);
+                }
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "End") 
+            {
+                buyers.FirstOrDefault(buyer => buyer.Name == input)?.BuyFood();
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(buyers.Sum(b => b.Food));
+        }
+    }
+}
