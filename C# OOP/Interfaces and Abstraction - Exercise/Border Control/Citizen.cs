36998dc668ec3c2d07dc8e0c6a57ef0e27cb87da
Anatoliy <leaved@abv.bg>
2023-06-29 14:20:06 +0300

    04. Border Control

---
+++ C# OOP/Interfaces and Abstraction - Exercise/Border Control/Citizen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorderControl
+{
+    public class Citizen : IIdentity
+    {
+        private string name;
+        private int age;
+        private string id;
+
+        public Citizen(string name, int age, string id)
+        {
+            this.name = name;
+            this.age = age;
+            this.Id = id;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            private set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            private set { this.age = value; }
+        }
+
+        public string Id
+        {
+            get { return this.id; }
+            private set { this.id = value; }
+        }
+    }
+}
---
+++ C# OOP/Interfaces and Abstraction - Exercise/Border Control/Program.cs
@@ -0,0 +1,52 @@
+﻿using BorderControl;
+
+namespace BorderControl
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            List<IIdentity> identities = new List<IIdentity>();
+            
+
+            while (command[0] != "End")
+            {
+                if (command.Length == 3)
+                {
+                    string name = command[0];
+                    int age = int.Parse(command[1]);
+                    string id = command[2];
+
+                    Citizen citizen = new Citizen(name, age, id);
+
+                    identities.Add(citizen);
+                }
+                else if (command.Length == 2) 
+                {
+                    string model = command[0];
+                    string id = command[1];
+
+                    Robot robot = new Robot(model, id);
+
+                    identities.Add(robot);
+                }
+
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            string fakeId = Console.ReadLine();
+
+            foreach (var identity in identities)
+            {
+                string lastDigits = identity.Id.Substring(identity.Id.Length - fakeId.Length);
+
+                if (lastDigits.Equals(fakeId, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"{identity.Id}");
+                }
+            }
+        }
+    }
+}
