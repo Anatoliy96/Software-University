0162d45794137142dd7baeb896e9c22a96da7424
Anatoliy <leaved@abv.bg>
2023-07-02 17:47:41 +0300

    02. Animals

---
+++ C# OOP/Polymorphism - Lab/Animals/Animal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favoriteFood;
+
+        public Animal(string name, string favoriteFood)
+        {
+            Name = name;
+            FavoriteFood = favoriteFood;
+        }
+
+        public string Name 
+        { 
+            get => name; 
+            private set => name = value; 
+        }
+        public string FavoriteFood 
+        { 
+            get => favoriteFood; 
+            private set => favoriteFood = value;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my fovourite food is {FavoriteFood}";
+        }
+
+        
+    }
+}
