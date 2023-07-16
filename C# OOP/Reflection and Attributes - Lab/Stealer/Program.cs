6ee7f1eac707cc124de2e0864a2754242a9bcf3c
Anatoliy <leaved@abv.bg>
2023-07-16 14:43:30 +0300

    02. High Quality Mistakes

--- C# OOP/Reflection and Attributes - Lab/Stealer/Program.cs
+++ C# OOP/Reflection and Attributes - Lab/Stealer/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Stealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Spy spy = new Spy();
+            string name = Console.ReadLine();
+
+            spy.AnalyzeAccessModifiers(name);
+        }
+    }
+}
--- C# OOP/Reflection and Attributes - Lab/Stealer/Spy.cs
+++ C# OOP/Reflection and Attributes - Lab/Stealer/Spy.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Stealer
 {
-    internal class Spy
+    public class Spy
     {
+        public string StealFieldInfo(string investigatedClass, params string[] requstedFields)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            FieldInfo[] fields = classType.GetFields(
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+
+            StringBuilder sb = new StringBuilder();
+
+            object classInstance = Activator.CreateInstance(classType, new object[] { });
+            sb.AppendLine($"Class under investigation: {investigatedClass}");
+
+            foreach (FieldInfo field in fields.Where(f => requstedFields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string AnalyzeAccessModifiers(string className)
+        {
+            Type classType = Type.GetType(className);
+            FieldInfo[] fields = classType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+            MethodInfo[] publicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo[] privateMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (FieldInfo field in fields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+            foreach (MethodInfo publicMethod in publicMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{publicMethod} have to be public!");
+            }
+            foreach (MethodInfo NonPublicMethod in privateMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{NonPublicMethod} have to be private!");
+            }
+            return sb.ToString().TrimEnd();
+        }
     }
 }
