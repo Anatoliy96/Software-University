fa462874027edf4699ee7177fbe0f6b9cbda8b0b
Anatoliy <leaved@abv.bg>
2023-07-18 20:11:52 +0300

    02. Validation Attributes

---
+++ C# OOP/Reflection and Attributes - Exercise/ValidationAttributes/MyRequiredAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationAttributes
+{
+    public class MyRequiredAttribute : MyValidationAttribute
+    {
+        public override bool IsValid(object obj)
+        {
+            if (obj is not null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
