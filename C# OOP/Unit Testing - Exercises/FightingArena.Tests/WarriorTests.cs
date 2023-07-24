438ba7498c9c7ed112cb7fce2630db315308d1e3
Anatoliy <leaved@abv.bg>
2023-07-24 19:53:19 +0300

    Test warrior constructor initilize correctly

--- C# OOP/Unit Testing - Exercises/FightingArena.Tests/WarriorTests.cs
+++ C# OOP/Unit Testing - Exercises/FightingArena.Tests/WarriorTests.cs
@@ -5,6 +5,25 @@ namespace FightingArena.Tests
     [TestFixture]
     public class WarriorTests
     {
+        private Warrior warrior;
 
+        [SetUp]
+        public void SetUp()
+        {
+            warrior = new Warrior("Gosho", 50, 100);
+        }
+
+        [Test]
+
+        public void ConstructorShouldInitilizeCorrectly()
+        {
+            string expectedName = "Gosho";
+            int expectedDamage = 50;
+            int expectedHealth = 100;
+
+            Assert.AreEqual(expectedName, warrior.Name);
+            Assert.AreEqual(expectedDamage, warrior.Damage);
+            Assert.AreEqual(expectedHealth, warrior.HP);
+        }
     }
 }
