4069eb88b3d30b9a654088e0ab4465a79d426a77
Anatoliy <leaved@abv.bg>
2023-07-25 16:26:31 +0300

    Arena Constructor test work correctly

--- C# OOP/Unit Testing - Exercises/FightingArena.Tests/ArenaTests.cs
+++ C# OOP/Unit Testing - Exercises/FightingArena.Tests/ArenaTests.cs
@@ -5,6 +5,19 @@
     [TestFixture]
     public class ArenaTests
     {
-        
+        private Arena arena;
+
+        [SetUp]
+        public void Setup()
+        {
+            arena = new Arena();
+        }
+
+        [Test]
+        public void ConstructorShouldWorkCorrectly()
+        {
+            Assert.IsNotNull(arena);
+            Assert.IsNotNull(arena.Warriors);
+        }
     }
 }
