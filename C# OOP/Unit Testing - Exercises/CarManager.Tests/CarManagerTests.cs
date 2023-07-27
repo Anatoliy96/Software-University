80e23adececc17a21a1a013d2e87182663c153f0
Anatoliy <leaved@abv.bg>
2023-07-27 18:02:49 +0300

    Car constructor test work correctly

--- C# OOP/Unit Testing - Exercises/CarManager.Tests/CarManagerTests.cs
+++ C# OOP/Unit Testing - Exercises/CarManager.Tests/CarManagerTests.cs
@@ -5,6 +5,36 @@ namespace CarManager.Tests
     [TestFixture]
     public class CarManagerTests
     {
+        private Car car;
 
+        [SetUp]
+        public void Setup()
+        {
+            car = new Car("Audi", "A3", 6, 40);
+        }
+
+        [Test]
+        public void CarConstructorShouldSetCorrectValues()
+        {
+            string expectedMake = "Audi";
+            string expectedModel = "A3";
+            double expectedFuelConsumption = 6;
+            double expectedFuelCapacity = 40;
+
+            car = new Car("Audi", "A3", 6, 40);
+
+            Assert.AreEqual(expectedMake, car.Make);
+            Assert.AreEqual(expectedModel, car.Model);
+            Assert.AreEqual(expectedFuelConsumption, car.FuelConsumption);
+            Assert.AreEqual(expectedFuelCapacity, car.FuelCapacity);
+        }
+
+        [Test]
+        public void CarBaseConstructorShouldSetCorrectValues()
+        {
+            double expectedFuelCapacity = 0;
+
+            Assert.AreEqual(expectedFuelCapacity, car.FuelAmount);
+        }
     }
 }
