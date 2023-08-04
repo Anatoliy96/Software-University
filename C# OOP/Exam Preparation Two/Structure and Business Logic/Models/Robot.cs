74f92e0b43118c42a2ee24d8a3e9f7fed84b27fa
Anatoliy <leaved@abv.bg>
2023-08-04 17:14:12 +0300

    01. Structure

---
+++ C# OOP/Exam Preparation Two/Structure and Business Logic/Models/Robot.cs
@@ -0,0 +1,116 @@
+﻿using RobotService.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Models
+{
+    public abstract class Robot : IRobot
+    {
+        private string model;
+        private int batteryCapacity;
+        private int convertionCapacityIndex;
+        private List<int> interfaceStandards;
+
+        public Robot(string model, int batteryCapacity, int convertionCapacityIndex)
+        {
+            this.Model = model;
+            this.BatteryCapacity = batteryCapacity;
+            this.ConvertionCapacityIndex = convertionCapacityIndex;
+
+            this.BatteryLevel = this.BatteryCapacity;
+            interfaceStandards = new List<int>();
+        }
+
+        public string Model
+        {
+            get => model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Model cannot be null or empty.");
+                }
+
+                model = value;
+            }
+        }
+
+        public int BatteryCapacity
+        {
+            get => batteryCapacity;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Battery capacity cannot drop below zero.");
+                }
+
+                batteryCapacity = value;
+            }
+        }
+
+        public int BatteryLevel 
+        { 
+            get => BatteryLevel; 
+            private set => BatteryLevel = value;
+        }
+
+        public int ConvertionCapacityIndex 
+        { 
+            get => convertionCapacityIndex; 
+            private set => convertionCapacityIndex = value; 
+        }
+
+
+        public IReadOnlyCollection<int> InterfaceStandards { get => interfaceStandards.AsReadOnly(); }
+
+        public void Eating(int minutes)
+        {
+            int totalCapacity = this.ConvertionCapacityIndex * minutes;
+
+            if (totalCapacity > this.BatteryCapacity - this.BatteryLevel) 
+            {
+                this.BatteryLevel = this.batteryCapacity;
+            }
+            else
+            {
+                this.batteryCapacity += totalCapacity;
+            }
+        }
+
+        public bool ExecuteService(int consumedEnergy)
+        {
+            if (this.BatteryLevel >= consumedEnergy)
+            {
+                this.BatteryLevel -= consumedEnergy;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void InstallSupplement(ISupplement supplement)
+        {
+            interfaceStandards.Add(supplement.InterfaceStandard);
+
+            this.batteryCapacity -= supplement.BatteryUsage;
+            this.BatteryLevel -= supplement.BatteryUsage;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            sb.AppendLine($"{this.GetType() == typeof(Robot)} {this.Model}:");
+            sb.AppendLine($"--Maximum battery capacity: {this.BatteryCapacity}");
+            sb.AppendLine($"--Current battery level: {this.BatteryLevel}");
+            sb.AppendLine($"--Supplements installed: {(interfaceStandards.Count != 0 ? string.Join(" ", interfaceStandards) : "none")}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
