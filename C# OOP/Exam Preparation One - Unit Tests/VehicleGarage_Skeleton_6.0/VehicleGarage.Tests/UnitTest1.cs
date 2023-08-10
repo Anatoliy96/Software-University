e95e7603e331c8a7a7bc29d6a657b142cbf2e65e
Anatoliy <leaved@abv.bg>
2023-08-10 13:46:08 +0300

    garage constructor work correctly

--- C# OOP/Exam Preparation One - Unit Tests/VehicleGarage_Skeleton_6.0/VehicleGarage.Tests/UnitTest1.cs
+++ C# OOP/Exam Preparation One - Unit Tests/VehicleGarage_Skeleton_6.0/VehicleGarage.Tests/UnitTest1.cs
@@ -5,11 +5,13 @@ namespace VehicleGarage.Tests
     public class Tests
     {
         private Vehicle vehicle;
+        private Garage garage;
 
         [SetUp]
         public void Setup()
         {
             vehicle = new Vehicle("Audi", "A3", "PB6771TT");
+            garage = new Garage(10);
         }
 
         [Test]
@@ -26,6 +28,18 @@ namespace VehicleGarage.Tests
             Assert.AreEqual(expectedLicensePlateNumber, vehicle.LicensePlateNumber);
             Assert.AreEqual(expectedBatteryLevel, vehicle.BatteryLevel);
             Assert.IsTrue(vehicle.IsDamaged == expectedIsDamage);
+        }
+
+        [Test]
+        public void GarageConstructorShouldWorkCorrectly()
+        {
+            int expectedGarageCapacity = 10;
+            int expectedListOfVehicles = 1;
+
+            garage.AddVehicle(vehicle);
+
+            Assert.AreEqual(expectedGarageCapacity, garage.Capacity);
+            Assert.IsTrue(expectedListOfVehicles == garage.Vehicles.Count);
         }
     }
 }
