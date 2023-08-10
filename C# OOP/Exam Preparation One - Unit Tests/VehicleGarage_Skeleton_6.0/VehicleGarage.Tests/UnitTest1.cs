3a0e194e45119aec8836183810cea83118965655
Anatoliy <leaved@abv.bg>
2023-08-10 13:39:27 +0300

    vehicle constructor work correctly

--- C# OOP/Exam Preparation One - Unit Tests/VehicleGarage_Skeleton_6.0/VehicleGarage.Tests/UnitTest1.cs
+++ C# OOP/Exam Preparation One - Unit Tests/VehicleGarage_Skeleton_6.0/VehicleGarage.Tests/UnitTest1.cs
@@ -4,15 +4,28 @@ namespace VehicleGarage.Tests
 {
     public class Tests
     {
+        private Vehicle vehicle;
+
         [SetUp]
         public void Setup()
         {
+            vehicle = new Vehicle("Audi", "A3", "PB6771TT");
         }
 
         [Test]
-        public void Test_1()
+        public void VehicleConstructorShouldWorkCorrectly()
         {
-            Assert.Pass();
+            string expectedBrand = "Audi";
+            string expectedModel = "A3";
+            string expectedLicensePlateNumber = "PB6771TT";
+            int expectedBatteryLevel = 100;
+            bool expectedIsDamage = false;
+
+            Assert.AreEqual(expectedBrand, vehicle.Brand);
+            Assert.AreEqual(expectedModel, vehicle.Model);
+            Assert.AreEqual(expectedLicensePlateNumber, vehicle.LicensePlateNumber);
+            Assert.AreEqual(expectedBatteryLevel, vehicle.BatteryLevel);
+            Assert.IsTrue(vehicle.IsDamaged == expectedIsDamage);
         }
     }
 }
