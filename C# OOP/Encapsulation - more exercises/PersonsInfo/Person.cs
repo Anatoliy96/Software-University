3114bcd132e05aae369fcbcc7d2e0e07bfcfeaca
Anatoliy <leaved@abv.bg>
2023-08-20 17:23:16 +0300

    03. Validation

--- C# OOP/Encapsulation - more exercises/PersonsInfo/Person.cs
+++ C# OOP/Encapsulation - more exercises/PersonsInfo/Person.cs
@@ -8,6 +8,11 @@ namespace PersonsInfo
 {
     public class Person
     {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
         public Person(string firstName, string lastName, int age, decimal salary)
         {
             FirstName = firstName;
@@ -16,10 +21,58 @@ namespace PersonsInfo
             Salary = salary;
         }
 
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public int Age { get; private set; }
-        public decimal Salary { get; set; }
+        public string FirstName
+        {
+            get => firstName;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+
+                lastName = value;
+            }
+        }
+        public int Age
+        {
+            get => age;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+
+                age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get => salary;
+            private set
+            {
+                if (value < 650)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+
+                salary = value;
+            }
+        }
 
         public void IncreaseSalary(decimal percentage)
         {
