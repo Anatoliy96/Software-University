64fda0a0254d339802178d9cbd52df3c45085dbb
Anatoliy <leaved@abv.bg>
2023-08-22 16:39:58 +0300

    pizza calories

---
+++ C# OOP/Encapsulation - more exercises/FootballTeamGenerator/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace FootballTeamGenerator
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            try
+            {
+                List<Team> teams = new List<Team>();
+
+                string[] command = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+                Team team = new Team(command[0]);
+
+                teams.Add(team);
+
+                while (command[0] != "END")
+                {
+                    string[] commands = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+                    Player player = new Player
+                        (
+                        commands[2],
+                        int.Parse(commands[3]),
+                        int.Parse(commands[4]),
+                        int.Parse(commands[5]),
+                        int.Parse(commands[6]),
+                        int.Parse(commands[7])
+                        );
+
+                    if (commands[0] == "Add")
+                    {
+                        if (teams.Contains(team))
+                        {
+                            team.AddPlayer(player);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Team {team.Name} does not exist.");
+                        }
+                    }
+                    else if (commands[0] == "Remove")
+                    {
+                        var playerToRemove = team.Players.FirstOrDefault(t => t.Name == commands[2]);
+
+                        if (playerToRemove != null)
+                        {
+                            team.RemovePlayer(playerToRemove);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Player {player.Name} is not in {team.Name} team.");
+                        }
+                    }
+                    else if (commands[0] == "Rating")
+                    {
+                        if (teams.Contains(team))
+                        {
+                            Console.WriteLine(team.Rating);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Team {team.Name} does not exist.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ C# OOP/Encapsulation - more exercises/FootballTeamGenerator/Team.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballTeamGenerator
+{
+    public class Team
+    {
+        private string name;
+        private readonly List<Player> players;
+
+        public Team(string name)
+        {
+            Name = name;
+            players = new List<Player>();
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+                name = value;
+            }
+        }
+        public double Rating
+        {
+            get
+            {
+                return Math.Round(players.Average(p => p.OverallSkill));
+            }
+        }
+
+        public IReadOnlyCollection<Player> Players { get => players.AsReadOnly(); }
+
+        public void AddPlayer(Player player)
+        {
+            players.Add(player);
+        }
+
+        public void RemovePlayer(Player player)
+        {
+            players.Remove(player);
+        }
+
+    }
+}
