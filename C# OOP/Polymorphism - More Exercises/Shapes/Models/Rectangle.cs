b56c2534d02b581d42618a32b8c49e5d8031d272
Anatoliy <leaved@abv.bg>
2023-11-11 11:43:58 +0200

    3.Shapes

---
+++ C# OOP/Polymorphism - More Exercises/Shapes/Models/Rectangle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Models
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double Height
+        {
+            get => height;
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Height cannot be zero");
+                }
+
+                height = value;
+            }
+        }
+        public double Width
+        {
+            get => width;
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Width cannot be zero");
+                }
+
+                width = value;
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            return Height * Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Height + Height + Width + Width;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + $" {typeof(Rectangle).Name}";
+        }
+    }
+}
---
+++ C# OOP/Polymorphism - More Exercises/Shapes/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Shapes.Models;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Rectangle rectangle = new Rectangle(10, 5.5);
+            Circle circle = new Circle(7.5);
+
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.CalculatePerimeter());
+            Console.WriteLine(rectangle.Draw());
+
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
