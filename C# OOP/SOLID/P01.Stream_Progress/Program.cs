6f838a73ef362513bfb3cb577590d03727e2d9e5
Anatoliy <leaved@abv.bg>
2023-11-21 14:22:02 +0200

    1. Stream Progress Info

---
+++ C# OOP/SOLID/P01.Stream_Progress/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+            StreamProgressInfo fileStream = new StreamProgressInfo(new File("txt", 5, 100));
+
+            Console.WriteLine(fileStream.CalculateCurrentPercent());
+
+            StreamProgressInfo musicStream = new StreamProgressInfo(new Music("Eminem", "Marshal LP", 5, 100));
+
+            Console.WriteLine(musicStream.CalculateCurrentPercent());
+        }
+    }
+}
---
+++ C# OOP/SOLID/P01.Stream_Progress/StreamProgressInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace P01.Stream_Progress
+{
+    public class StreamProgressInfo
+    {
+        private IStreamProgressInfo streamProgress;
+
+        // If we want to stream a music file, we can't
+        public StreamProgressInfo(IStreamProgressInfo streamProgressInfo)
+        {
+            this.streamProgress = streamProgressInfo;
+        }
+
+        public int CalculateCurrentPercent()
+        {
+            return (this.streamProgress.BytesSent * 100) / this.streamProgress.Length;
+        }
+    }
+}
