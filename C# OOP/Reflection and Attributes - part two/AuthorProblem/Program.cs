7680a25eab08df68f75d0f216e061ad6563583ef
Anatoliy <leaved@abv.bg>
2023-11-23 12:52:00 +0200

    06. Code Tracker

--- C# OOP/Reflection and Attributes - part two/AuthorProblem/Program.cs
+++ C# OOP/Reflection and Attributes - part two/AuthorProblem/Program.cs
@@ -6,7 +6,9 @@
         [Author("George")]
         public static void Main()
         {
-            
+            var tracker = new Tracker();
+
+            tracker.PrintMethodsByAuthor();
         }
     }
 }
