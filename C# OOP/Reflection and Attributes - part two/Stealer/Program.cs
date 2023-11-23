043487116a89d317bbef799f69b83b3962dba5bf
Anatoliy <leaved@abv.bg>
2023-11-23 11:34:02 +0200

    01. Stealer

---
+++ C# OOP/Reflection and Attributes - part two/Stealer/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Stealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Spy spy = new Spy();
+
+            string result = spy.StealFieldInfo("Stealer.Hacker", new string[] { "username", "password" });
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C# OOP/Reflection and Attributes - part two/Stealer/Spy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string name, string[] fieldsToInvestigate)
+        {
+            Type type = typeof(Hacker);
+
+            FieldInfo[] fields = type.GetFields((BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static));
+            
+            StringBuilder stringBuilder = new StringBuilder();
+            object classInstace = Activator.CreateInstance(type, new object[] { });
+
+            stringBuilder.AppendLine($"Class under investigation: {name}");
+            foreach (FieldInfo field in fields.Where(f => fieldsToInvestigate.Contains(f.Name)))
+            {
+                stringBuilder.AppendLine($"{field.Name} = {field.GetValue(classInstace)}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
