9ef3b425ead3d7db8c3107129c80a7082fa1a004
Anatoliy <leaved@abv.bg>
2023-11-30 19:51:15 +0200

    01. Structure

---
+++ C# OOP/Exam Preparation-Part Two/BookingApp/Models/Hotels/Hotel.cs
@@ -0,0 +1,64 @@
+﻿using BookingApp.Models.Bookings;
+using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Repositories;
+using BookingApp.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Models.Hotels
+{
+    public class Hotel : IHotel
+    {
+        private string fullName;
+        private int category;
+        
+        public Hotel(string fullName, int category)
+        {
+            FullName = fullName;
+            Category = category;
+            Rooms = new RoomRepository();
+            Bookings = new BookingRepository();
+        }
+
+        public string FullName
+        {
+            get => fullName; private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Hotel name cannot be null or empty!");
+                }
+                fullName = value;
+            }
+        }
+
+        public int Category
+        {
+            get => category; private set
+            {
+                if (value < 1 || value > 5)
+                {
+                    throw new ArgumentException("Category should be between 1 and 5 stars!");
+                }
+                category = value;
+            }
+        }
+
+        public double Turnover 
+        {
+            get
+            {
+                return Math.Round(Bookings.All().Sum(b => b.ResidenceDuration * b.Room.PricePerNight));
+            }
+        }
+
+        public IRepository<IRoom> Rooms { get; private set; }
+
+        public IRepository<IBooking> Bookings { get; private set; }
+    }
+}
