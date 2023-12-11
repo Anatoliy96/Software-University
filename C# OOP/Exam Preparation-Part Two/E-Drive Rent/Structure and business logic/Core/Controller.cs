3fedfd7286fae8caa738158e19908d2f80e08162
Anatoliy <leaved@abv.bg>
2023-12-11 13:30:38 +0200

    02. Business Logic

---
+++ C# OOP/Exam Preparation-Part Two/E-Drive Rent/Structure and business logic/Core/Controller.cs
@@ -0,0 +1,177 @@
+﻿using EDriveRent.Core.Contracts;
+using EDriveRent.Models;
+using EDriveRent.Models.Contracts;
+using EDriveRent.Repositories;
+using EDriveRent.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDriveRent.Core
+{
+    public class Controller : IController
+    {
+        private IRepository<IUser> users;
+        private IRepository<IVehicle> vehicles;
+        private IRepository<IRoute> routes;
+
+        public Controller()
+        {
+            users = new UserRepository();
+            vehicles = new VehicleRepository();
+            routes = new RouteRepository();
+        }
+
+        public string AllowRoute(string startPoint, string endPoint, double length)
+        {
+            IRoute route = routes.GetAll()
+                .FirstOrDefault(r => r.StartPoint == startPoint && r.EndPoint == endPoint && r.Length == length);
+
+            if (route != null)
+            {
+                return $"{startPoint}/{endPoint} - {length} km is already added in our platform.";
+            }
+
+            route = routes.GetAll()
+                .FirstOrDefault(r => r.StartPoint == startPoint && r.EndPoint == endPoint && r.Length < length);
+
+            if (route != null)
+            {
+                return $"{startPoint}/{endPoint} shorter route is already added in our platform.";
+            }
+
+            route = routes.GetAll()
+                .FirstOrDefault(r => r.StartPoint == startPoint && r.EndPoint == endPoint && r.Length > length);
+
+            if (route != null)
+            {
+                route.LockRoute();
+            }
+
+            route = new Route(startPoint, endPoint, length, routes.GetAll().Count + 1);
+
+            routes.AddModel(route);
+
+            return $"{startPoint}/{endPoint} - {length} km is unlocked in our platform.";
+        }
+
+        public string MakeTrip(string drivingLicenseNumber, string licensePlateNumber, string routeId, bool isAccidentHappened)
+        {
+            IUser user = users.FindById(drivingLicenseNumber);
+            IVehicle vehicle = vehicles.FindById(licensePlateNumber);
+            IRoute route = routes.FindById(routeId);
+
+            if (user.IsBlocked == true)
+            {
+                return $"User {drivingLicenseNumber} is blocked in the platform! Trip is not allowed.";
+            }
+
+            if (vehicle.IsDamaged == true)
+            {
+                return $"Vehicle {licensePlateNumber} is damaged! Trip is not allowed.";
+            }
+
+            if (route.IsLocked == true)
+            {
+                return $"Route {routeId} is locked! Trip is not allowed.";
+            }
+
+            vehicle.Drive(route.Length);
+
+            if (isAccidentHappened == true)
+            {
+                vehicle.ChangeStatus();
+                user.DecreaseRating();
+            }
+            else
+            {
+                user.IncreaseRating();
+            }
+
+            return vehicle.ToString();
+        }
+
+        public string RegisterUser(string firstName, string lastName, string drivingLicenseNumber)
+        {
+            IUser user = users.FindById(drivingLicenseNumber);
+
+            if (user != null)
+            {
+                return $"{drivingLicenseNumber} is already registered in our platform.";
+            }
+
+            user = new User(firstName, lastName, drivingLicenseNumber);
+
+            users.AddModel(user);
+
+            return $"{firstName} {lastName} is registered successfully with DLN-{drivingLicenseNumber}";
+        }
+
+        public string RepairVehicles(int count)
+        {
+            int repairsCount = 0;
+
+            var damagedVehicles = vehicles.GetAll()
+                .Where(v => v.IsDamaged == true)
+                .OrderBy(v => v.Brand)
+                .ThenBy(v => v.Model)
+                .Take(count)
+                .ToList();
+
+            foreach (var vehicle in damagedVehicles)
+            {
+                vehicle.ChangeStatus();
+                vehicle.Recharge();
+
+                repairsCount++;
+            }
+
+            return $"{repairsCount} vehicles are successfully repaired!";
+        }
+
+        public string UploadVehicle(string vehicleType, string brand, string model, string licensePlateNumber)
+        {
+            if (vehicleType != typeof(CargoVan).Name && vehicleType != typeof(PassengerCar).Name)
+            {
+                return $"{vehicleType} is not accessible in our platform";
+            }
+
+            IVehicle vehicle = vehicles.FindById(licensePlateNumber);
+
+            if (vehicle != null)
+            {
+                return $"{licensePlateNumber} belongs to another vehicle.";
+            }
+
+            if (vehicleType == typeof(CargoVan).Name)
+            {
+                vehicle = new CargoVan(brand, model, licensePlateNumber);
+            }
+            else if (vehicleType == typeof(PassengerCar).Name)
+            {
+                vehicle = new PassengerCar(brand, model, licensePlateNumber);
+            }
+
+            vehicles.AddModel(vehicle);
+
+            return $"{brand} {model} is uploaded successfully with LPN-{licensePlateNumber}";
+        }
+
+        public string UsersReport()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("*** E-Drive-Rent ***");
+
+            foreach (var user in users.GetAll().OrderByDescending(u => u.Rating).ThenBy(u => u.LastName).ThenBy(u => u.FirstName)) 
+            {
+                sb.AppendLine(user.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
