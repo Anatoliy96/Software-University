4d39bbaaf75df0521c95c4f64f77d629e90405e6
Anatoliy <leaved@abv.bg>
2023-12-14 18:52:22 +0200

    implement all the models

---
+++ C# OOP/Exam Preparation-Part Two/Handball/Structure and business logic/Handball/Models/Goalkeeper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Models
+{
+    public class Goalkeeper : Player
+    {
+        private const double rating = 2.5;
+
+        public Goalkeeper(string name) 
+            : base(name, rating)
+        {
+        }
+
+        public override void DecreaseRating()
+        {
+            base.Rating -= 1.25;
+
+            if (Rating < 1)
+            {
+                base.Rating = 1;
+            }
+        }
+
+        public override void IncreaseRating()
+        {
+            base.Rating += 0.75;
+
+            if (Rating > 10)
+            {
+                base.Rating = 10;
+            }
+        }
+    }
+}
---
+++ C# OOP/Exam Preparation-Part Two/Handball/Structure and business logic/Handball/Models/Team.cs
@@ -0,0 +1,87 @@
+﻿using Handball.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Models
+{
+    public class Team : ITeam
+    {
+        private List<IPlayer> players;
+        private string name;
+        
+        public Team(string name)
+        {
+            Name = name;
+            PointsEarned = 0;
+            players = new List<IPlayer>();
+        }
+
+        public string Name
+        {
+            get => name; 
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Team name cannot be null or empty.");
+                }
+                name = value;
+            }
+        }
+
+        public int PointsEarned { get; private set; }
+
+        public double OverallRating
+        {
+            get
+            {
+                if (!players.Any())
+                {
+                    return 0;
+                }
+                else
+                {
+                    return Math.Round(players.Average(p => p.Rating), 2);
+                }
+            }
+            
+        }
+
+        public IReadOnlyCollection<IPlayer> Players => players.AsReadOnly();
+
+        public void Draw()
+        {
+            PointsEarned += 1;
+
+            IPlayer player = players.FirstOrDefault(p => p.GetType().Name == typeof(Goalkeeper).Name);
+
+            player.IncreaseRating();
+        }
+
+        public void Lose()
+        {
+            foreach (var player in players)
+            {
+                player.DecreaseRating();
+            }
+        }
+
+        public void SignContract(IPlayer player)
+        {
+            players.Add(player);
+        }
+
+        public void Win()
+        {
+            PointsEarned += 3;
+
+            foreach (var player in players)
+            {
+                player.IncreaseRating();
+            }
+        }
+    }
+}
